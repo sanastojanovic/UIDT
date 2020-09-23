5b8bac552381e11d3bb0b6f94e3ede019a662f14
jbranislav05 <56685782+jbranislav05@users.noreply.github.com>
2020-09-23 18:02:38 +0200

    prerasporedjivanje

--- uidt_2020_seminarski/mi16093_Jovanovic_Branislav.thy
+++ uidt_2020_seminarski/mi16093_Jovanovic_Branislav.thy
@@ -2,23 +2,52 @@ theory mi16093_Jovanovic_Branislav
   imports Complex_Main
 begin
 
-text\<open>
+text‹
  Prvi zadatak A kategorije cetvrtog razreda sa takmicenja:
  https://imomath.com/srb/zadaci/2016_opstinsko.pdf
  
 Tekst zadatka: Neka su a i b prirodni brojevi, pri cemu
  vazi a>b. Dokazati nejednakost:
   
- a^3 + a*(b^2) + b^2 + b \<ge> 2*(a^2)*b + a^2.
+ a^3 + a*(b^2) + b^2 + b ≥ 2*(a^2)*b + a^2.
  
- \<close>
- (* Prvi deo dokaza *)
+ ›
 
-(* Niz pomocnih lema za dokazivanje da je, kada prebacimo sve
-na jednu stranu, S = a^3 + a*b^2 + b^2 + b − 2*a^2*b − a^2 zapravo
-jednako (a^2 - a*b - b)*(a - b - 1) *)
 
-(*Leme p_1, p_2, p_3 dokazuju redom svaki deo proizvoda sa (a - b - 1)*)
+(*   Prvi deo dokaza   *)
+
+(*      Niz pomocnih lema za dokazivanje da je, kada prebacimo sve
+        na jednu stranu, S = a^3 + a*b^2 + b^2 + b − 2*a^2*b − a^2 zapravo
+        jednako (a^2 - a*b - b)*(a - b - 1)                                         *)
+
+
+(*    Leme koje dokazuju medjukorak izmedju 
+      a^3 - a^2*b - a^2 - a^2*b + a*b^2 + a*b - a*b + b^2 + b i (a^2 - a*b - b) * (a -b -1)     *)
+
+lemma medjukorak_1:
+  fixes a b :: nat
+  assumes "a>b"
+  shows "a^3 + a*(b^2) + b^2 + b - 2*(a^2)*b - a^2 = (a^3 - (a^2)*b-a^2)-((a^2)*b - a*(b^2) - a*b) - (a*b - b^2 - b)"
+proof-
+  have "a^3 + a*(b^2) + b^2 + b - 2*(a^2)*b - a^2 = a^3 + a*(b^2) + b^2 + b - (a^2)*b -(a^2)*b - a^2" by auto
+  also have "... = a^3 + a*(b^2) + b^2 + b - (a^2)*b - (a^2)*b - a^2 + a*b - a*b" by auto
+  also have "... =  a^3  - (a^2)*b  - a^2 + a*(b^2) - (a^2)*b + a*b + b^2 + b - a*b" by auto
+
+  qed
+
+
+(*********************************************************)
+lemma kontra:
+  fixes a b :: nat
+  assumes "a>b"
+  shows "a^3 + a*(b^2) + b^2 + b - (a^2)*b - (a^2)*b - a^2  = a^3  - (a^2)*b  - a^2 + a*(b^2) - (a^2)*b + a*b + b^2 + b - a*b" by auto  
+
+(*********************************************************)
+
+
+
+(*      Pomocne leme za medjukorak 2      *)
+(*    Leme p_1, p_2, p_3 dokazuju redom svaki deo proizvoda sa (a - b - 1)       *)
 
 lemma p_1:
   fixes a b :: nat
@@ -39,36 +68,11 @@ lemma p_3:
   by (simp add: diff_mult_distrib2 mult.commute semiring_normalization_rules(29))
 
 
-(*Pomocna lema za medjukorak*)
-
-lemma medjukorak_2_pomocna:
+lemma medjukorak_2_spajanje:
   fixes a b :: nat
   assumes "a > b"
   shows "a^2*(a - b -1) - a*b*(a - b - 1) - b*(a-b-1) = (a^2 - a*b - b) * (a -b -1)"
   using diff_mult_distrib by presburger
-
-(*Leme koje dokazuju medjukorak izmedju 
-a^3 - a^2*b - a^2 - a^2*b + a*b^2 + a*b - a*b + b^2 + b
-i (a^2 - a*b - b) * (a -b -1)  *)
-
-
-lemma medjukorak_1:
-  fixes a b :: nat
-  assumes "a>b"
-  shows "a^3 + a*(b^2) + b^2 + b - 2*(a^2)*b - a^2 = (a^3 - (a^2)*b-a^2)-((a^2)*b - a*(b^2) - a*b) - (a*b - b^2 - b)"
-proof-
-  have "a^3 + a*(b^2) + b^2 + b - 2*(a^2)*b - a^2 = a^3 + a*(b^2) + b^2 + b - (a^2)*b -(a^2)*b - a^2" by auto
-  also have "... = a^3 + a*(b^2) + b^2 + b - (a^2)*b - (a^2)*b - a^2 + a*b - a*b" by auto
-  
-  qed
-  
-(*********************************************************)
-lemma kontra:
-  fixes a b :: nat
-  assumes "a>b"
-  shows "a^3 + a*(b^2) + b^2 + b - (a^2)*b - (a^2)*b - a^2  = a^3  - (a^2)*b  - a^2 + a*(b^2) - (a^2)*b + a*b + b^2 + b - a*b" by auto  
-
-(*********************************************************)
 
 lemma medjukorak_2:
   fixes a b :: nat
@@ -80,17 +84,17 @@ proof-
     using assms(1) p_1 p_2 p_3 by auto
 
   also have "... = (a^2 - a*b -b) * (a -b -1)"
-    using assms(1) medjukorak_2_pomocna by blast
+    using assms(1) medjukorak_2_spajanje by blast
 
   finally show ?thesis by blast
   
 qed
 
 
-(*Drugi deo dokaza gde dokazujemo S≥0*)
+(*      Drugi deo dokaza gde dokazujemo S≥0.       *)
 
-(*Lema koja iz a>b, gde su a i b prirodni brojevi
-dokazuje da je drugi clan proizvoda a - b -1 ≥0*)
+(*        Lema koja iz a>b, gde su a i b prirodni brojevi
+          dokazuje da je drugi clan proizvoda a - b -1 ≥0.       *)
 
 lemma s_2:
   fixes a b :: nat
@@ -98,8 +102,11 @@ lemma s_2:
   shows "a - b - 1 ≥ 0"
   by blast
 
-(*Pomocna lema pri dokazu da je prvi clan proizvoda S
-(a^2 - a*b - b) veci od 0*)
+
+(*        Pomocna lema pri dokazu da je prvi clan proizvoda S
+          (a^2 - a*b - b) veci od 0.                                       *)
+
+
 lemma s_1_1:
   fixes a b :: nat 
   assumes "a>b"
@@ -124,7 +131,14 @@ lemma s_1_4:
   shows "a - b > 0"
   using assms by simp
 
-(*Zadatak*)
+(*  Ili kraće zapisano samo   *)
+lemma S_1:
+  fixes a b :: nat 
+  assumes "a>b"
+  shows "a^2 - a*b - b ≥ 0" by simp
+  
+
+(*  Zadatak  *)
 lemma zadatak:
   fixes a b :: nat
   assumes "a > b" 
