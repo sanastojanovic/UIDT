5c81b46c91380b994d12eb45b451f84ba5a6c6a9
Alienso <varsa191@gmail.com>
2021-05-26 17:49:55 +0200

    Nadjen uslov zaustavljanja

--- uidt_2021_seminarski/mi17039_Vladimir_Arsenijevic.thy
+++ uidt_2021_seminarski/mi17039_Vladimir_Arsenijevic.thy
@@ -5,7 +5,7 @@ begin
 text‹
   https://www.imo-official.org/problems/IMO2018SL.pdf
 
-  3.zadatak
+  Zadatak C4
   Antipaskalov trougaoje tablica u obliku jednakostraniqnog trouglakoja se sastoji od brojeva tako da, osim za brojeve u poslednjem redu, vazi da je svakibroj jednak apsolutnoj vrednosti razlike dva broja koja su neposredno ispod njega. Da li postoji antipaskalov trougao sa 2018 redova koji se sastoji od svih prirodnihbrojeva od 1 do 1 + 2 +···+ 2018?
 ›
 
@@ -19,30 +19,40 @@ text‹
 2 4 9 6 2 5 3 3 2 1
 *)
 
-fun vrednost :: "int list\<Rightarrow>int list" where
-"vrednost [] = []" |
-"vrednost [x] = [x]" |
-"vrednost [x,y] = [abs(x-y)]" |
-"vrednost (x#y#xs) = vrednost (y#xs) @ [abs(x-y)]"
+fun red_gore :: "int list\<Rightarrow>int list" where
+"red_gore [x,y] = [abs(x-y)]" |
+"red_gore (x#y#xs) = red_gore (y#xs) @ [abs(x-y)]"
 
 lemma len_vrednost_manje_len_x:
-  shows "length (vrednost x) \<le> length x"
+  shows "length (red_gore x) < length x"
   apply(induction x)
-   apply fastforce
   sorry
 
-fun vrednost' :: "int list \<Rightarrow> int list \<Rightarrow> int list" where
+(*function vrednost' :: "int list \<Rightarrow> int list \<Rightarrow> int list" where
 "vrednost' x [] = []" |
 "vrednost' x [y] = [y]" |
-"vrednost' x y = x@(vrednost' (vrednost y) (vrednost y))"
+"vrednost' x y = x@(vrednost' (vrednost y) (vrednost y))"*)
+
+function trougao' :: "int list \<Rightarrow> int list \<Rightarrow> int list" where
+"trougao' l1 l2 =
+  (if l2 = [] then []
+  else if (length l2) = 1 then l2
+  else l1@((trougao' (rev (red_gore l2)) (rev (red_gore l2)))))"
+  by pat_completeness auto
+  termination
+  apply (relation "measure (\<lambda> (a, b). length b)")
+    apply(auto simp add: len_vrednost_manje_len_x)
+    done
+
+fun trougao :: "int list \<Rightarrow>int list" where
+"trougao x = x@(trougao' [] x)"
 
 value "length []"
-value "vrednost [2::int,4,9,6]"
-value "vrednost [3::int,5,2]"
-value "vrednost [3::int,2]"
-value "vrednost' [] [2::int,4,9,6]"
+value "red_gore [2::int,4,9,6]"
+value "red_gore [3::int,5,2]"
+value "red_gore [3::int,2]"
+value "trougao' [] [2::int,4,9,6]"
+value "trougao [2::int,4,9,6]"
 
-fun antiPaskal :: "int list \<Rightarrow> int list" where
-"antiPaskal x = vrednost' [] x"
 
 end
