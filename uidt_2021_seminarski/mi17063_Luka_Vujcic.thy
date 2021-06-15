1bb5ede1edc2bada7cd3377b745c8d9c7697a002
Luka Vujcic <lukavujcic@hotmail.com>
2021-06-15 22:57:49 +0200

    Dodat tekst zadatka

---
+++ uidt_2021_seminarski/mi17063_Luka_Vujcic.thy
@@ -0,0 +1,14 @@
+theory mi17063_Luka_Vujcic
+  imports Main
+begin
+
+text\<open>
+  https://www.imo-official.org/problems/IMO2013SL.pdf
+  Zadatak: Number theory N2
+  Tekst zadatka: Dokazati da za svaka dva prirodna broja k i n postoji
+  k prirodnih brojeva m1,m2,...,mk (ne obavezno razlicitih) takvih da je
+  1+ (2^k-1)/n=(1+1/m1)(1+1/m2)...(1+1/mk)
+\<close>
+
+
+end
