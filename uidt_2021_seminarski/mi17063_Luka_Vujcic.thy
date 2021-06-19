25c267bb177da6e56e6012c6818daafdd2f719c8
Luka Vujcic <lukavujcic@hotmail.com>
2021-06-19 19:10:42 +0200

    Dodata formulacija leme

--- uidt_2021_seminarski/mi17063_Luka_Vujcic.thy
+++ uidt_2021_seminarski/mi17063_Luka_Vujcic.thy
@@ -1,7 +1,6 @@
 theory mi17063_Luka_Vujcic
-  imports Main
+  imports Complex_Main
 begin
-
 text\<open>
   https://www.imo-official.org/problems/IMO2013SL.pdf
   Zadatak: Number theory N2
@@ -9,6 +8,11 @@ text\<open>
   k prirodnih brojeva m1,m2,...,mk (ne obavezno razlicitih) takvih da je
   1+ (2^k-1)/n=(1+1/m1)(1+1/m2)...(1+1/mk)
 \<close>
-
-
+(*Formulacija leme*)
+lemma
+  fixes k n::nat
+  assumes "k>0" "n>0" 
+  shows "\<exists> m::nat list. prod_list m \<noteq> 0 \<and> length m=k \<and> 1+ (2^k-1) / n=sum_list (map (\<lambda> x. 1+1 / x) m)"
+  using [[show_types]]
+  sorry
 end
