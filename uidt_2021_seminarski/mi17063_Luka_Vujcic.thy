8fea402251d23f2f8483aa00aaf34c2471b287b5
Luka Vujcic <lukavujcic@hotmail.com>
2021-06-25 17:54:46 +0200

    Dodata induktivna baza dokaza

--- uidt_2021_seminarski/mi17063_Luka_Vujcic.thy
+++ uidt_2021_seminarski/mi17063_Luka_Vujcic.thy
@@ -13,6 +13,27 @@ lemma
   fixes k n::nat
   assumes "k>0" "n>0" 
   shows "\<exists> m::nat list. prod_list m \<noteq> 0 \<and> length m=k \<and> 1+ (2^k-1) / n=prod_list (map (\<lambda> x. 1+1 / x) m)"
-  using [[show_types]]
-  sorry
+proof (induction k)
+  case 0
+    then show ?case by simp
+next
+  case (Suc k)
+  show ?case
+  proof (cases "Suc k=1")
+    case True
+    note k_value=this
+    show ?thesis
+    proof-
+      obtain m where "m=[n::nat]" by  auto
+      note m_lista=this
+      then have "prod_list m \<noteq> 0"  "length m = Suc k" "1 + (2 ^ Suc k - 1) / real n = (\<Prod>x\<leftarrow>m. 1 + 1 / real x)" using  m_lista k_value m_lista assms  by auto
+      then show ?thesis by auto
+    qed
+  next
+    case False
+    then show ?thesis sorry
+  qed
+qed
+
+
 end
