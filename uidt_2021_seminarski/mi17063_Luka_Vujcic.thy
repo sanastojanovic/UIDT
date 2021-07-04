d602d382edf6e2704ad8a227337c1f5bc6ac2b55
Luka Vujcic <lukavujcic@hotmail.com>
2021-07-04 16:26:55 +0200

    Zavrsen slucaj kad je n neparan

--- uidt_2021_seminarski/mi17063_Luka_Vujcic.thy
+++ uidt_2021_seminarski/mi17063_Luka_Vujcic.thy
@@ -15,8 +15,6 @@ lemma
   shows "\<exists> m::nat list. prod_list m \<noteq> 0 \<and> length m=k \<and> 1+ (2^k-1) / n=prod_list (map (\<lambda> x. 1+1 / x) m)"
   sorry
 
-
-
 (*Seminarski  2*)
 lemma
   fixes k n::nat
@@ -76,7 +74,34 @@ proof (induction k arbitrary:n)
       qed
     next
       case False
-      then show ?thesis sorry
+      then show ?thesis
+      proof-
+        obtain t where "n=2*t-1" using False by (metis add_diff_cancel_right' evenE even_plus_one_iff)
+        show "\<exists>m. prod_list m \<noteq> 0 \<and> length m = Suc k \<and> 1 + (2 ^ Suc k - 1) / real n = (\<Prod>x\<leftarrow>m. 1 + 1 / real x) "
+        proof-
+          obtain m where "prod_list m\<noteq>0 \<and> length m=k \<and> 1+(2^k-1)/real (t)= (\<Prod>x\<leftarrow>m. 1 + 1 / real x)" using Suc.IH \<open>n = 2 * t - 1\<close> ih(3) by fastforce
+          have "1 + (2 ^ Suc k - 1) / real n = 1+ (2* 2^k -1 ) / real n" by auto
+          also have "\<dots>= 1+ (2* 2^k -1 ) / real (2*t-1)"using \<open>n = 2 * t - 1\<close> by blast
+          also have "\<dots>=(2*t-1)/real (2*t-1)+ (2* 2^k -1 ) / real (2*t-1)" using \<open>n = 2 * t - 1\<close> ih(3) by auto
+          also have "\<dots>=(2* 2^k -1+2*t-1 ) / real (2*t-1)" using add_diff_inverse_nat add_divide_distrib calculation less_one nat.simps(3) of_nat_Suc of_nat_add of_nat_power one_add_one plus_1_eq_Suc power_Suc2 power_commutes power_eq_0_iff by (smt (verit, best) One_nat_def \<open>1 + (2 * 2 ^ k - 1) / real (2 * t - 1) = real (2 * t - 1) / real (2 * t - 1) + (2 * 2 ^ k - 1) / real (2 * t - 1)\<close> \<open>n = 2 * t - 1\<close> add_is_0 ih(3) nat_add_left_cancel_less of_nat_mult zero_less_diff)
+          also have "\<dots>=(2* 2^k -1+2*t-1 )/real(2*t)*(2*t)/real(2*t-1)" by simp
+          also have "\<dots>=(2*2^k +2*t-2 )/real(2*t)*(2*t)/real(2*t-1)" by auto
+          also have "\<dots>=2*(2^k +t-1 )/real(2*t)*(2*t)/real(2*t-1)" by auto
+          also have "\<dots>=(2^k +t-1 )/real(t)*(2*t)/real(2*t-1)" by auto
+          also have "\<dots>=(2^k +t-1 )/real(t)*(2*t-1+1)/real(2*t-1)" by (metis One_nat_def Suc_eq_plus1 \<open>n = 2 * t - 1\<close> add_diff_inverse_nat ih(3) less_one not_add_less1 plus_1_eq_Suc zero_less_diff)
+          also have "\<dots>=(2^k +t-1 )/real(t)*((2*t-1)/real(2*t-1)+1/real(2*t-1))" by (metis (no_types, lifting) add_divide_distrib of_nat_1 of_nat_add times_divide_eq_right)
+          also have "\<dots>=(2^k +t-1 )/real(t)*(1+1/real(2*t-1))" by auto
+          also have "\<dots>=(t/real(t)+(2^k-1 )/real(t))*(1+1/real(2*t-1))" using algebra_simps by (smt (verit) \<open>n = 2 * t - 1\<close> add.right_neutral add_diff_cancel_left add_diff_inverse_nat add_divide_distrib add_less_cancel_right add_mono_thms_linordered_field(5) cancel_comm_monoid_add_class.diff_cancel ih(3) less_one mult_cancel_right nat_1_add_1 neq0_conv of_nat_1 of_nat_add of_nat_power zero_less_diff)
+          also have "\<dots>=(1+(2^k-1 )/real(t))*(1+1/real(2*t-1))" using \<open>n = 2 * t - 1\<close> ih(3) by force
+          also have "\<dots>=(1+1/real(2*t-1))*(\<Prod>x\<leftarrow>m. 1 + 1 / real x)" by (simp add: \<open>prod_list m \<noteq> 0 \<and> length m = k \<and> 1 + (2 ^ k - 1) / real t = (\<Prod>x\<leftarrow>m. 1 + 1 / real x)\<close>)
+          also have "\<dots>=(\<Prod>x\<leftarrow>(2*t-1)#m. 1 + 1 / real x)" by auto
+          let ?l="(2*t-1)#m"
+          have 1:"prod_list ?l \<noteq> 0" by (metis \<open>n = 2 * t - 1\<close> \<open>prod_list m \<noteq> 0 \<and> length m = k \<and> 1 + (2 ^ k - 1) / real t = (\<Prod>x\<leftarrow>m. 1 + 1 / real x)\<close> ih(3) mult_eq_0_iff nat_neq_iff prod_list.Cons)
+          have 2: "length ?l = Suc k" by (simp add: \<open>prod_list m \<noteq> 0 \<and> length m = k \<and> 1 + (2 ^ k - 1) / real t = (\<Prod>x\<leftarrow>m. 1 + 1 / real x)\<close>)
+          have 3: "1 + (2 ^ Suc k - 1) / real n = (\<Prod>x\<leftarrow>?l. 1 + 1 / real x)" using \<open>(1 + 1 / real (2 * t - 1)) * (\<Prod>x\<leftarrow>m. 1 + 1 / real x) = (\<Prod>x\<leftarrow>(2 * t - 1) # m. 1 + 1 / real x)\<close> calculation by presburger
+          from 1 2 3 show ?thesis by blast
+        qed
+      qed
     qed
   qed
 qed
