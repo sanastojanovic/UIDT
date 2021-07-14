2ba1477d9bb544ee2b3d8ddb22b1a084ab6c9157
AizenAngel <boza.mitrovic@gmail.com>
2021-07-14 11:56:59 +0200

    Divide file on 2 seminar assignments

--- uidt_2021_seminarski/mi17054_Bozidar_Mitrovic.thy
+++ uidt_2021_seminarski/mi17054_Bozidar_Mitrovic.thy
@@ -11,53 +11,93 @@ text\<open>
   Prove that a + b + c + d < b/a + c/b + d/c + a/d
 \<close>
 
+(*Seminarski 1*)
+
+lemma
+  fixes a b c d :: real
+  assumes  "a>0" "b>0" "c>0" "d>0" "a * b * c * d = 1" "a + b + c + d > a / b + b / c + c / d + d / a"
+  shows "a + b + c + d < b /a + c / b + d / c + a / d"
+  sorry
 
 
-
-lemma argme2:
-  fixes a b :: real
-  assumes "a \<ge> 0" "b \<ge> 0"
-  shows "a + b \<ge>  2 * root 2 (a *b)"
-proof(-) 
-  have "0 \<le> (sqrt a - sqrt b)^2"
-    by auto
-  also have "... = (sqrt a - sqrt b)*(sqrt a - sqrt b)"
-    by (simp add: power2_eq_square)
-  also have "... = sqrt a * (sqrt a - sqrt b) - sqrt b * (sqrt a - sqrt b)"
-    by (auto simp:left_diff_distrib) 
-  also have "... = (sqrt a * sqrt a - sqrt a * sqrt b) - sqrt b * (sqrt a - sqrt b)"
-    by (metis left_diff_distrib mult.commute)
-  also have  "... = (a - sqrt a * sqrt b) - sqrt b * (sqrt a - sqrt b)"
-    using assms(1) 
-    by auto
-  also have  "... = a - sqrt a * sqrt b - sqrt b * (sqrt a - sqrt b)"
-    by auto
-  also have "... = a - sqrt a * sqrt b - sqrt b * sqrt a + sqrt b * sqrt b"
-    by (simp add: right_diff_distrib')
-  also have "... = a - sqrt a * sqrt b - sqrt b * sqrt a + b"
-    using assms(2) 
-    by auto
-  also have "... = a - sqrt a * sqrt b - sqrt a * sqrt b + b"
-    by (auto simp:mult.commute)
-  also have "... = a - 2 * sqrt a * sqrt b + b"
-    by auto
-  finally show ?thesis
-    by (smt (verit) \<open>(sqrt a - sqrt b) * (sqrt a - sqrt b) = sqrt a * (sqrt a - sqrt b) - sqrt b * (sqrt a - sqrt b)\<close> \<open>(sqrt a - sqrt b)\<^sup>2 = (sqrt a - sqrt b) * (sqrt a - sqrt b)\<close> \<open>0 \<le> (sqrt a - sqrt b)\<^sup>2\<close> \<open>a - sqrt a * sqrt b - sqrt a * sqrt b + b = a - 2 * sqrt a * sqrt b + b\<close> \<open>sqrt a * (sqrt a - sqrt b) - sqrt b * (sqrt a - sqrt b) = sqrt a * sqrt a - sqrt a * sqrt b - sqrt b * (sqrt a - sqrt b)\<close> \<open>sqrt a * sqrt a - sqrt a * sqrt b - sqrt b * (sqrt a - sqrt b) = a - sqrt a * sqrt b - sqrt b * (sqrt a - sqrt b)\<close> real_sqrt_mult sqrt_def)
-qed
-
+(*Seminarski 2*)
 
 lemma root2_eq_sqrt:
   assumes "x \<ge> 0"
   shows "root 2 (x * y) = sqrt (x * y)"
   using sqrt_def by auto
 
-
 lemma transform_root_2_2:
   shows "root 2 (root 2 (x)) = root 4 (x)"
   by (metis num_double numeral_times_numeral real_root_mult_exp)
 
+lemma add_ineq4:
+  fixes a b c d :: real
+  assumes "a + b \<le> c + d" "a > c"
+  shows "b < d"
+  using assms(1) assms(2) by linarith
 
-lemma argme_4_help_lemma:
+lemma mult_ineq4:
+  fixes a b c d :: real
+  assumes "a>0" "b>0" "c>0" "d>0" "a + b + c + d \<le> (b /a + c / b + d / c + a / d)"
+  shows "(3*a/4 + 3*b/4 + 3*c/4 + 3*d/4) \<le> (3 *b /a + 3*c / b +3* d / c + 3*a / d) / 4"
+  using assms(5)
+  by auto
+
+lemma right_div_distrib4:
+  fixes a b c d :: real
+  shows "(a/4 + b/4 + c/4 + d/4) = (a + b + c + d) / 4"
+  by auto
+
+lemma div_ineq2:
+  fixes a b :: real
+  assumes "a / 4 < b / 4"
+  shows "a < b"
+  using assms
+  using [[show_types]]
+  by auto
+
+lemma div_ineq4:
+  fixes a b c d :: real
+  assumes "(a + b + c + d)/ 4 < (b/a + c/b + d/c + a/d)/ 4"
+  shows "(a + b + c + d) < (b/a + c/b + d/c + a/d)"
+  using assms
+  using div_ineq2[of "(a + b + c + d)" "(b/a + c/b + d/c + a/d)"]
+  by blast
+  
+lemma argme2:
+  fixes a b :: real
+  assumes "a \<ge> 0" "b \<ge> 0"
+  shows "a + b \<ge>  2 * root 2 (a *b)"
+proof(-) 
+  have 1:"0 \<le> (sqrt a - sqrt b)^2"
+    by auto
+  also have 2:"... = (sqrt a - sqrt b)*(sqrt a - sqrt b)"
+    by (simp add: power2_eq_square)
+  also have 3:"... = sqrt a * (sqrt a - sqrt b) - sqrt b * (sqrt a - sqrt b)"
+    by (auto simp:left_diff_distrib) 
+  also have 4:"... = (sqrt a * sqrt a - sqrt a * sqrt b) - sqrt b * (sqrt a - sqrt b)"
+    by (metis left_diff_distrib mult.commute)
+  also have  5:"... = (a - sqrt a * sqrt b) - sqrt b * (sqrt a - sqrt b)"
+    using assms(1) 
+    by auto
+  also have  6:"... = a - sqrt a * sqrt b - sqrt b * (sqrt a - sqrt b)"
+    by auto
+  also have 7:"... = a - sqrt a * sqrt b - sqrt b * sqrt a + sqrt b * sqrt b"
+    by (simp add: right_diff_distrib')
+  also have 8:"... = a - sqrt a * sqrt b - sqrt b * sqrt a + b"
+    using assms(2) 
+    by auto
+  also have 9:"... = a - sqrt a * sqrt b - sqrt a * sqrt b + b"
+    by (auto simp:mult.commute)
+  also have 10:"... = a - 2 * sqrt a * sqrt b + b"
+    by auto
+  finally show ?thesis
+    using 1 2 3 4 5 6 7 8 9 10 assms
+    by (smt (verit) real_sqrt_mult root2_eq_sqrt) 
+qed
+
+lemma argme4_help_lemma:
   fixes a b c d :: real
   assumes "a \<ge> 0" "b \<ge> 0" "c \<ge> 0" "d \<ge> 0"
   shows "2 * root 2 ((2 * root 2 (a * b)) *  (2 * root 2 (c * d))) = 4 * root 4 (a * b * c * d)"
@@ -80,30 +120,30 @@ proof(-)
     by (metis calculation mult.assoc real_root_mult transform_root_2_2)
 qed
 
-
 lemma argme4:
   fixes a b c d ::real
   assumes "a \<ge> 0" "b \<ge>0" "c \<ge> 0" "d\<ge>0"
   shows "root 4 (a*b*c*d) \<le> 1/4 * (a + b + c + d)"
   using assms
 proof(-)
-  have "a + b \<ge> 2 * root 2 (a * b)"
+  have 1:"a + b \<ge> 2 * root 2 (a * b)"
     using assms
     by (auto simp:argme2)
-  also have "c + d \<ge> 2 * root 2 (c * d)"
+  also have 2:"c + d \<ge> 2 * root 2 (c * d)"
     using assms
     by (auto simp:argme2)
-  hence  "a + b + c + d \<ge> 2 * root 2 (a * b) + 2 * root 2 (c * d)" 
+  hence  3:"a + b + c + d \<ge> 2 * root 2 (a * b) + 2 * root 2 (c * d)" 
     using calculation by linarith
-  have "(2 * root 2 (a * b)) + (2 * root 2 (c * d)) \<ge> 2 * root 2 ((2 * root 2 (a * b)) *  (2 * root 2 (c * d)))"
+  have 4:"(2 * root 2 (a * b)) + (2 * root 2 (c * d)) \<ge> 2 * root 2 ((2 * root 2 (a * b)) *  (2 * root 2 (c * d)))"
     using assms
     by (smt (verit) argme2 mult_nonneg_nonneg real_root_pos_pos_le)
   show ?thesis
-    using \<open>2 * root 2 (2 * root 2 (a * b) * (2 * root 2 (c * d))) \<le> 2 * root 2 (a * b) + 2 * root 2 (c * d)\<close> \<open>2 * root 2 (a * b) + 2 * root 2 (c * d) \<le> a + b + c + d\<close> argme_4_help_lemma assms(1) assms(2) assms(3) assms(4) by auto
+    using 1 2 3 4 argme4_help_lemma assms(1) assms(2) assms(3) assms(4) 
+    by auto
 qed
 
 
-lemma main_proof_help_lemma1:
+lemma main_proof_help_lemma_1:
   fixes a b c d :: real
   assumes  "a>0" "b>0" "c>0" "d>0" "a * b * c * d = 1"
   shows "a \<le>  1/4 * ((a/b) + (a/b) + (b/c) + (a/d))"
@@ -171,42 +211,7 @@ proof-
     by auto
 qed
 
-lemma add_ineq:
-  fixes a b c d :: real
-  assumes "a + b \<le> c + d" "a > c"
-  shows "b < d"
-  using assms(1) assms(2) by linarith
-
-lemma div_ineq1:
-  fixes a b c d :: real
-  assumes "a>0" "b>0" "c>0" "d>0" "a + b + c + d \<le> (b /a + c / b + d / c + a / d)"
-  shows "(3*a/4 + 3*b/4 + 3*c/4 + 3*d/4) \<le> (3 *b /a + 3*c / b +3* d / c + 3*a / d) / 4"
-  using assms(5)
-  by auto
-
-lemma right_div_distrib:
-  fixes a b c d :: real
-  shows "(a/4 + b/4 + c/4 + d/4) = (a + b + c + d) / 4"
-  by auto
-
-lemma div_ineq2:
-  fixes a b :: real
-  assumes "a / 4 < b / 4"
-  shows "a < b"
-  using assms
-  using [[show_types]]
-  by auto
-
-lemma new_test_1:
-  fixes a b c d :: real
-  assumes "(a + b + c + d)/ 4 < (b/a + c/b + d/c + a/d)/ 4"
-  shows "(a + b + c + d) < (b/a + c/b + d/c + a/d)"
-  using assms
-  using div_ineq2[of "(a + b + c + d)" "(b/a + c/b + d/c + a/d)"]
-  by blast
-  
-
-lemma main_proof_help_lemma2:
+lemma main_proof_help_lemma_2:
   fixes a b c d :: real
   assumes  "a>0" "b>0" "c>0" "d>0" "a * b * c * d = 1" "a + b + c + d > a / b + b / c + c / d + d / a" "a + b + c + d \<le> (3 * (a/b) + 3 * b/c + 3 * c/d + 3 * d/a) / 4 +  (b/a + c/b + d/c + a/d)/ 4"
   shows "a + b + c + d < b /a + c / b + d / c + a / d"
@@ -218,17 +223,17 @@ proof(-)
     by auto
   also have "(a/4 + b/4 + c/4 + d/4) < (b/a + c/b + d/c + a/d)/ 4"
     using assms
-    using div_ineq1
-    using add_ineq[of "(3*a/4 + 3*b/4 + 3*c/4 + 3*d/4)"                              
+    using mult_ineq4
+    using add_ineq4[of "(3*a/4 + 3*b/4 + 3*c/4 + 3*d/4)"                              
                       "(a/4 + b/4 + c/4 + d/4)"
                       "(3 * (a/b) + 3 * (b/c) + 3 * (c/d) + 3 * (d/a)) / 4"
                       "(b/a + c/b + d/c + a/d) / 4"]  
     by auto
   from this have "(a + b + c + d)/ 4 < (b/a + c/b + d/c + a/d)/ 4"
-    using right_div_distrib
+    using right_div_distrib4
     by auto
   then show ?thesis
-    using new_test_1
+    using div_ineq4
     by meson
 qed
 
@@ -238,24 +243,25 @@ lemma main_lemma:
   assumes  "a>0" "b>0" "c>0" "d>0" "a * b * c * d = 1" "a + b + c + d > a / b + b / c + c / d + d / a"
   shows "a + b + c + d < b /a + c / b + d / c + a / d"
 proof-
-  have "a \<le> 1/4 * ((a/b) + (a/b) + (b/c) + (a/d))"
+  have 1:"a \<le> 1/4 * ((a/b) + (a/b) + (b/c) + (a/d))"
     using assms
-    using main_proof_help_lemma1 
+    using main_proof_help_lemma_1 
     by auto
-  have "b \<le> 1/4 * ((b/c) + (b/c) + (c/d) + (b/a))"
+  have 2:"b \<le> 1/4 * ((b/c) + (b/c) + (c/d) + (b/a))"
     using assms
-    using main_proof_help_lemma1[of b c d a]
+    using main_proof_help_lemma_1[of b c d a]
     by (metis mult.assoc mult.commute)
-  have "c \<le> 1/4 * ( (c/d) + (c/d) + (d/a) + (c/b))"
+  have 3:"c \<le> 1/4 * ( (c/d) + (c/d) + (d/a) + (c/b))"
     using assms
-    using main_proof_help_lemma1[of c d a b]
+    using main_proof_help_lemma_1[of c d a b]
     by (metis mult.assoc mult.commute)
-  have "d \<le> 1/4 * ((d/a) + (d/a) + (a/b) + (d/c))"
+  have 4:"d \<le> 1/4 * ((d/a) + (d/a) + (a/b) + (d/c))"
     using assms
-    using main_proof_help_lemma1[of d a b c]
+    using main_proof_help_lemma_1[of d a b c]
     by (metis mult.assoc mult.commute)
   have "a + b + c + d \<le> 1/4 * ((a/b) + (a/b) + (b/c) + (a/d)) + 1/4 * ((b/c) + (b/c) + (c/d) + (b/a)) + 1/4 * ( (c/d) + (c/d) + (d/a) + (c/b)) + 1/4 * ((d/a) + (d/a) + (a/b) + (d/c))"
-    using \<open>a \<le> 1 / 4 * (a / b + a / b + b / c + a / d)\<close> \<open>b \<le> 1 / 4 * (b / c + b / c + c / d + b / a)\<close> \<open>c \<le> 1 / 4 * (c / d + c / d + d / a + c / b)\<close> \<open>d \<le> 1 / 4 * (d / a + d / a + a / b + d / c)\<close> by linarith
+    using 1 2 3 4
+    by linarith
   also have  "... = (3 * (a/b) + 3 * b/c + 3 * c/d + 3 * d/a) / 4 +  (b/a + c/b + d/c + a/d)/ 4"
     using assms(1) assms(2) assms(3) assms(4)
     using calc_help_lemma
@@ -264,7 +270,7 @@ proof-
     using calculation by presburger
   thus ?thesis
     using assms
-    using main_proof_help_lemma2
+    using main_proof_help_lemma_2
     by auto
 qed
 end
