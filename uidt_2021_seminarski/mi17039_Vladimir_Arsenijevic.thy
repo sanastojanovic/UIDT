7c3515ea885bcfa28c8b01014fba4e09dbbbe033
Alienso <varsa191@gmail.com>
2021-09-16 14:13:15 +0200

    Doradjeno na vise teorema

--- uidt_2021_seminarski/mi17039_Vladimir_Arsenijevic.thy
+++ uidt_2021_seminarski/mi17039_Vladimir_Arsenijevic.thy
@@ -19,7 +19,6 @@ text‹
 2 4 9 6 2 5 3 3 2 1
 *)
 
-
 primrec sublist_pom :: "'a list ⇒ nat ⇒ nat ⇒ nat ⇒ 'a list" where
 "sublist_pom [] n m i = []" |
 "sublist_pom (x#xs) n m i = (if i<n then (sublist_pom xs n m (i+1)) else (
@@ -122,18 +121,25 @@ value "sabirci_n [2::nat,4,9,6]"
 lemma an_mora_da_bude_suma_1_do_n:
   assumes "je_resenje l"
   assumes "length l = 10"
+  assumes "n < length(l)-1"
+  shows "sum_list (sabirci_n l n) = sum_list [1..10]"
+  sorry
+
+(*lemma an_mora_da_bude_suma_1_do_n:
+  assumes "je_resenje l"
+  assumes "length l = 10"
   assumes "n ≤ length(l)"
   assumes "sum_list (sabirci_n l n) = sum_list [1..10]"
   assumes "an ∈ set l"
-  assumes "an > 10"
+  assumes "an > 55"
   shows "False"
 proof-
   from ‹je_resenje l› have "(set(trougao l) = set([1..55]))"
     using assms(1) je_resenje_def by blast
   from this and ‹an ∈ set l› have "an ∈ set (trougao l)" sorry (*ako je u l treba da je i u trougao l*)
   from this and ‹(set(trougao l) = set([1..55]))› have "an ∈ set [1..55]" by blast
-  from this and ‹an > 10› show ?thesis sorry (*Ne zna da se svaki broj pojavljuje jednom*)
-qed
+  from this and ‹an > 55› show ?thesis sorry (*Ne zna da se svaki broj pojavljuje jednom*)
+qed*)
 
 value "trougao [2::int,4,9,6]"
 value "sabirci_n (trougao [2::int,4,9,6]) 2"
@@ -146,11 +152,89 @@ lemma duzina_manje_je_bar_pola:
   assumes "l' = sublist l 0 (i-1)"
   assumes "l'' = sublist l (j+1) (length l-1)"
   assumes "lp = (if length l' > length l'' then l' else l'')"
-  shows "length lp ≥ (length l - 2) / 2"
+  shows "length lp ≥ ⌈(length l - 2) / 2⌉"
   sorry
 
+primrec suma' :: "nat ⇒ nat" where
+"suma' 0 = 0" |
+"suma' (Suc n) = suma' n + (Suc n)"
+
+lemma suma_n_n_1_2: "suma' n = n * (n + 1) div 2"
+  apply (induction n)
+   apply simp +
+  done
+
+definition suma:: "nat⇒nat⇒nat" where
+"suma n m = suma' m - suma' (n-1)"
+
+lemma ako_je_jedan_veci_od_sum_1_n_false:
+  assumes "x>55"
+  assumes "je_resenje l"
+  assumes "length l = 10"
+  assumes "n < length(l)-1"
+  shows "False"
+  sorry
+
+
+lemma pom1:
+  fixes n::nat
+  assumes "2*l≥ (n-2)"
+  assumes "n>0"
+  shows "2 * suma (n+1) (n+l) = l*(2*n + l + 1)"
+proof(induction n)
+  case 0
+  then show ?case
+  proof-
+    have "2 * suma (0 + 1) (0 + l) = 2*suma 1 l" by auto
+    then have "... = 2 * (suma' l - suma' 0)" by (simp add: suma_def)
+    then have "... = 2 * ((l * (l+1) div 2) + 0)" by (simp add: suma_n_n_1_2)
+    then have "... = 2 * (l * (l+1) div 2)" by auto
+    then have "... = l * (l+1)" by auto
+    then show ?case using ‹2 * (suma' l - suma' 0) = 2 * (l * (l + 1) div 2 + 0)› ‹2 * suma 1 l = 2 * (suma' l - suma' 0)› by auto
+  qed
+next
+  case (Suc n)
+  then show ?case
+  proof-
+    have "2 * suma (Suc n + 1) (Suc n + l) = 2 * suma (n + 2) (n + l + 1)" by auto
+    then have "... = 2* (suma' (n+l+1) - suma' (n+1))" by (metis Suc_1 Suc_eq_plus1 add_Suc_shift add_diff_cancel_right' suma_def)
+    then have "... = 2 * (suma' (n+l) + (n+l+1) - suma' (n+1))" using Suc_eq_plus1 suma'.simps(2) by presburger
+    then have "... = 2 * (suma' (n+l) + n+l+1 - (suma' n + n+1))" using Suc_eq_plus1 suma'.simps(2) by presburger
+    then have "... = 2 * (suma' (n+l) + n+l+1 - suma' n - (n+1))" using diff_diff_left by presburger
+    then have "... = 2* (suma' (n+l) - suma' n + n+l+1 - (n+1))" sorry
+    then have "... = 2*(suma (n+1) (n+l) + n+l+1 - (n+1))" by (metis add_diff_cancel_right' suma_def)
+    then have "... = 2*(suma (n+1) (n+l) + n+l+1 - n -1)" using diff_diff_left by presburger
+    then have "... = 2*(suma (n+1) (n+l) + l)" by auto
+    then have "... = 2* suma (n+1) (n+l) + 2*l" by auto
+    then have "... = l * (2 * n + l + 1) + 2*l" using Suc.IH by presburger
+    then have "... = l * (2*n + l + 3)" by (metis One_nat_def Suc_1 add.commute add_Suc_shift add_mult_distrib2 mult.commute numeral_3_eq_3 plus_1_eq_Suc)
+    then have "... = l*(2*n+2 + l+ 1)" by (metis One_nat_def Suc_1 Suc_eq_plus1 add.commute group_cancel.add1 numeral_3_eq_3)
+    then have "... = l*(2*(n+1) + l+1)" by simp
+    then show ?case using Suc_eq_plus1 ‹2 * (suma (n + 1) (n + l) + l) = 2 * suma (n + 1) (n + l) + 2 * l› ‹2 * (suma (n + 1) (n + l) + n + l + 1 - (n + 1)) = 2 * (suma (n + 1) (n + l) + n + l + 1 - n - 1)› ‹2 * (suma (n + 1) (n + l) + n + l + 1 - n - 1) = 2 * (suma (n + 1) (n + l) + l)› ‹2 * (suma' (n + l + 1) - suma' (n + 1)) = 2 * (suma' (n + l) + (n + l + 1) - suma' (n + 1))› ‹2 * (suma' (n + l) + (n + l + 1) - suma' (n + 1)) = 2 * (suma' (n + l) + n + l + 1 - (suma' n + n + 1))› ‹2 * (suma' (n + l) + n + l + 1 - (suma' n + n + 1)) = 2 * (suma' (n + l) + n + l + 1 - suma' n - (n + 1))› ‹2 * (suma' (n + l) + n + l + 1 - suma' n - (n + 1)) = 2 * (suma' (n + l) - suma' n + n + l + 1 - (n + 1))› ‹2 * (suma' (n + l) - suma' n + n + l + 1 - (n + 1)) = 2 * (suma (n + 1) (n + l) + n + l + 1 - (n + 1))› ‹2 * suma (Suc n + 1) (Suc n + l) = 2 * suma (n + 2) (n + l + 1)› ‹2 * suma (n + 1) (n + l) + 2 * l = l * (2 * n + l + 1) + 2 * l› ‹2 * suma (n + 2) (n + l + 1) = 2 * (suma' (n + l + 1) - suma' (n + 1))› ‹l * (2 * n + l + 1) + 2 * l = l * (2 * n + l + 3)› ‹l * (2 * n + l + 3) = l * (2 * n + 2 + l + 1)› by presburger
+  qed
+qed
+
+lemma pom2:
+  fixes n :: real
+  assumes "n>0"
+  shows "(n - 2) / 2 * (2 * n + (n - 2) / 2 + 1) / 2 =  5*n*(n-2)/8"
+proof-
+  have "(n - 2) / 2 * (2 * n + (n - 2) / 2 + 1) / 2 = (n - 2) * (2 * n + (n - 2) / 2 + 1) / 4" by auto
+  then have "... = (n - 2) * ((4 * n + (n - 2)) / 2 + 1) / 4" by auto
+  then have "... = (n - 2) * ((4 * n + (n - 2) + 2) / 2) / 4" sorry
+  then have "... = (n - 2) * (4 * n + (n - 2) + 2) / 8" by auto
+  then have "... = (n * (4 * n + (n - 2) + 2) - 2 * (4 * n + (n - 2) + 2)) / 8" using int_distrib(3) diff_mult_distrib mult.commute sorry
+  then have "... = ((n * 4 * n + n * (n - 2) + n*2) - 2 * (4 * n + (n - 2) + 2)) / 8" by auto
+  then have "... = ((n * 4 * n + n * (n - 2) + n*2) - (2*4 * n + 2*(n - 2) + 4)) / 8" by auto
+  then have "... = (n * 4 * n + n * (n - 2) + n*2 - 2*4 * n - 2*(n - 2) - 4) / 8" by auto
+  then have "... = (n*4*n + n*n - 2*n + n*2 - 2*4 * n - 2*n + 2*2 - 4) / 8" sorry
+  then have "... = (4*n*n + n*n - 2*n + 2*n - 8 * n - 2*n + 4 - 4) / 8" by auto
+  then have "... = (5*n*n - 10*n)/8" by auto
+  then show ?thesis by (smt (z3) ‹(n - 2) * ((4 * n + (n - 2) + 2) / 2) / 4 = (n - 2) * (4 * n + (n - 2) + 2) / 8› ‹(n - 2) * ((4 * n + (n - 2)) / 2 + 1) / 4 = (n - 2) * ((4 * n + (n - 2) + 2) / 2) / 4› ‹(n - 2) * (2 * n + (n - 2) / 2 + 1) / 4 = (n - 2) * ((4 * n + (n - 2)) / 2 + 1) / 4› ‹(n - 2) / 2 * (2 * n + (n - 2) / 2 + 1) / 2 = (n - 2) * (2 * n + (n - 2) / 2 + 1) / 4› mult.commute)
+qed
+
 lemma resenje:
-  assumes "je_resenje (trougao l)"
+  assumes "je_resenje l"
   assumes "length l = 10"
   shows "False"
 proof -
@@ -161,23 +245,36 @@ proof -
   obtain "bn" where "bn = l!j" by auto
   obtain "l'" where "l' = sublist l 0 (i-1)" by auto
   obtain "l''" where "l'' = sublist l (j+1) (length l - 1)" by auto
-  obtain "lp" where "lp = (if length l' > length l'' then l' else l'')" by auto (*length lp > n-2/2*)
+  obtain "lp" where "lp = (if length l' > length l'' then l' else l'')" by auto
 
   obtain "i'" where "i'<i ∧ i'≥0"
     using ‹i < length l - 1 ∧ 0 < i› by blast
   obtain "j'" where "j' = i'+1" by auto
   obtain "ak" where "ak = l!i'" by auto
   obtain "bk" where "bk = l!j'" by auto
-  with duzina_manje_je_bar_pola have "length lp ≥ (length l - 2) / 2"
+  with duzina_manje_je_bar_pola have "length lp ≥ ⌈(length l - 2) / 2⌉"
     using ‹i < length l - 1 ∧ 0 < i› ‹j = i + 1› ‹l' = sublist l 0 (i - 1)› ‹l'' = sublist l (j + 1) (length l - 1)› ‹lp = (if length l'' < length l' then l' else l'')› assms(2) by blast
-  with this and an_mora_da_bude_suma_1_do_n have "bk ≥ sum_list [(length l)+1..(length l)+(length lp)]"
+  with this and an_mora_da_bude_suma_1_do_n have "bk ≥ suma ((length l)+1) ((length l)+(length lp))"
     sorry
+  from ‹length lp ≥⌈(length l - 2) / 2⌉› have "2 * length lp ≥ (length l - 2)" by linarith
 
-  then have "bk = (length lp)*(2*(length l) + (length lp) + 1) / 2" sorry
-  then have "bk ≥ ((length l)-2) / 4 * (2*(length l) + ((length l)-2)/2 + 1)" sorry
-  then have "bk = 5*(length l)*((length l)-2)/8" sorry
-  then have "bk ≥ sum_list [1..10]" sorry
-  then show ?thesis sorry
+  have "2 * suma ((length l)+1) ((length l)+(length lp)) = (length lp)*(2*(length l) + (length lp) + 1)" using ‹length l = 10› ‹2 * length lp ≥ (length l - 2)› pom1
+    by (metis zero_less_numeral)
+  then have "suma (length l + 1) (length l + length lp) = ((length lp)*(2*(length l) + (length lp) + 1)) / 2" by linarith
+  then have "bk≥((length lp)*(2*(length l) + (length lp) + 1)) / 2"
+    using ‹int (suma (length l + 1) (length l + length lp)) ≤ bk› by linarith
+  then have "... ≥ (length l - 2) / 2 *  (2 * length l + (length l - 2) / 2 + 1) div 2" using ‹length lp ≥ ⌈(length l - 2) / 2⌉› sorry
+  then have "(length l - 2) / 2 *  (2 * length l + (length l - 2) / 2 + 1) / 2 =  5*(length l)*((length l)-2)/8" using pom2 by auto
+  then have "... > 55" using ‹length l = 10› sorry (*ne vazi za 10 ali vazi za 2018*)
+
+  have "bk≥ (length l - 2) / 2 *  (2 * length l + (length l - 2) / 2 + 1) div 2"
+    using ‹real (length l - 2) / 2 * (real (2 * length l) + real (length l - 2) / 2 + 1) / 2 ≤ real_of_int bk› by blast
+  then have "bk≥ 5*(length l)*((length l)-2)/8"
+    using ‹real (length l - 2) / 2 * (real (2 * length l) + real (length l - 2) / 2 + 1) / 2 = real (5 * length l * (length l - 2)) / 8› sorry (*by presburger nalazi resenje sledgehammer ali ne prolazi*)
+  then have "bk>55" using ‹length l = 10› ‹55 < real (5 * length l * (length l - 2)) / 8› by auto
+  have "i<length l - 1" using ‹i<length l - 1 ∧ i>0› by auto
+  then show ?thesis using ako_je_jedan_veci_od_sum_1_n_false ‹bk>55› ‹je_resenje l› ‹length l = 10› ‹i<length l - 1›
+    by blast
 qed
 
 end
