d7f59763300f52bc9ebab489bfb08d307635eeee
erlierine <ana97markovic@gmail.com>
2021-09-30 15:35:32 +0200

    Promena indeksa

--- uidt_2021_seminarski/mi16127_Ana_Markovic.thy
+++ uidt_2021_seminarski/mi16127_Ana_Markovic.thy
@@ -66,14 +66,14 @@ definition S_4k_3 :: "nat \<Rightarrow> int" where
 
 (* S_4k = 2*S_k *)
 lemma S_4k_eq_2S_k:
-  assumes "n\<ge>4"
+  assumes "n\<ge>1"
   shows "S_4k n = 2 * S_k n"
   using assms
   sorry
 
 (* S_4k+2 = S_4k *)
 lemma S_4k_eq_S_4k_2:
-  assumes "n\<ge>6"
+  assumes "n\<ge>1"
   shows "S_4k_2 n = S_4k n"
   using assms
   sorry
@@ -88,7 +88,7 @@ lemma S_4k_eq_S_4k_2:
 *)
 
 lemma S_4k_nonegativ:
-  assumes "n \<ge> 4"
+  assumes "n \<ge> 1"
   assumes "S_k n \<ge> 0"
   shows "S_4k n \<ge> 0"
   using assms
@@ -97,7 +97,7 @@ proof(induction n rule: nat_less_induct)
   then show ?case 
   proof-
     have "S_4k n = 2 * S_k n"
-      using S_4k_eq_2S_k `n \<ge> 4` by simp
+      using S_4k_eq_2S_k `n \<ge> 1` by simp
     also have "... \<ge> 0"
       using 1 by simp
     finally show ?case .
@@ -105,7 +105,7 @@ proof(induction n rule: nat_less_induct)
 qed
 
 lemma S_4k_2_nonegativ:
-  assumes "n \<ge> 6"
+  assumes "n \<ge> 1"
   assumes "S_k n \<ge> 0"
   shows "S_4k_2 n \<ge> 0"
   using assms
@@ -114,7 +114,7 @@ proof(induction n rule: nat_less_induct)
   then show ?case 
   proof-
     have "S_4k_2 n = S_4k n"
-      using S_4k_eq_S_4k_2 `n \<ge> 6` by simp
+      using S_4k_eq_S_4k_2 `n \<ge> 1` by simp
     also have "... \<ge> 0"
       using 1 S_4k_nonegativ by simp
     finally show ?case .
@@ -122,7 +122,7 @@ proof(induction n rule: nat_less_induct)
 qed
 
 lemma S_4k_4_nonegativ:
-  assumes "n \<ge> 6"
+  assumes "n \<ge> 1"
   assumes "S_k (n+1) \<ge> 0"
   shows "S_4k_4 n \<ge> 0"
   using assms
@@ -139,7 +139,7 @@ proof(induction n rule: nat_less_induct)
 qed
 
 lemma S_4k_3_nonegativ:
-  assumes "n \<ge> 6"
+  assumes "n \<ge> 1"
   assumes "S_k n \<ge> 0"
   shows "S_4k_3 n \<ge> 0"
   using assms
@@ -148,16 +148,32 @@ proof(induction n rule: nat_less_induct)
   then show ?case sorry
 qed
 
+definition S_4k_1 :: "nat \<Rightarrow> int" where
+"S_4k_1 n = S_4k n + x_i_def (4*n+1)"
+
+definition S_4_k_1 :: "nat \<Rightarrow> int" where
+"S_4_k_1 k = S_4k k + S_4k_1 k + S_4k_2 k + S_4k_3 k +  S_4k_4 k"
+
+lemma S_4k_eq_S_4_k_1 :
+  assumes "n\<ge>1"
+  shows "S_4k n = S_4_k_1 (n-1)"
+  sorry
+
+
 lemma sum_k:
   fixes k::nat
   assumes "n \<le> (4*k)"
-  assumes "n \<ge> 6"
-  shows "S_k n \<ge> 0"
+  assumes "n \<ge> 1"
+  shows "S_4k n \<ge> 0"
   using assms
-proof (induction n rule: nat_less_induct)
-  case (1 n)
+proof (induction n)
+case 0
+then show ?case by simp
+next
+  case (Suc n)
   then show ?case 
     sorry
 qed
+
 
 end
