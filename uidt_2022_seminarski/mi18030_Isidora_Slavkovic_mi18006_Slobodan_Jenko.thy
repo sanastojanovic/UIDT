2f3ef083d84aa612901ded6c44272225c011a2e4
Isidora Slavkovic <isidora.slavkovic@gmail.com>
2022-07-02 17:41:21 +0200

    Defined first part of Field section.

--- uidt_2022_seminarski/mi18030_Isidora_Slavkovic_mi18006_Slobodan_Jenko.thy
+++ uidt_2022_seminarski/mi18030_Isidora_Slavkovic_mi18006_Slobodan_Jenko.thy
@@ -56,8 +56,81 @@ theorem T1_11:
   shows "(\<exists> \<alpha> \<in> S. (supremum \<alpha> L S) \<and> (infimum \<alpha> B S))"
   sorry
 
+section \<open>Polje\<close>
 
+text\<open>1.12 Definicija\<close>
+locale Polje =
+  fixes F :: "'a set"
+  fixes plus :: "'a \<Rightarrow> 'a  \<Rightarrow> 'a" (infixl "\<oplus>" 100)
+  fixes puta :: "'a \<Rightarrow> 'a  \<Rightarrow> 'a" (infixl "\<otimes>" 101)
+  fixes nula :: "'a"
+  fixes jedan :: "'a"
+  fixes inverz_plus :: "'a \<Rightarrow> 'a"
+  fixes inverz_puta :: "'a \<Rightarrow> 'a"
 
+  assumes A1: "\<And> x y. \<lbrakk>x \<in> F; y \<in> F\<rbrakk> \<Longrightarrow> x \<oplus> y \<in> F"
+  assumes A2: "\<And> x y. \<lbrakk>x \<in> F; y \<in> F\<rbrakk> \<Longrightarrow> x \<oplus> y = y \<oplus> x"
+  assumes A3: "\<And> x y. \<lbrakk>x \<in> F; y \<in> F\<rbrakk> \<Longrightarrow> (x \<oplus> y) \<oplus> z = x \<oplus> (y \<oplus> z)"
+  assumes A4: "nula \<in> F \<and> (\<forall> x \<in> F. nula \<oplus> x = x)"
+  assumes A5: "\<And> x. \<lbrakk>x \<in> F\<rbrakk> \<Longrightarrow> (inverz_plus x) \<in> F \<and> x \<oplus> (inverz_plus x) = nula"
+
+  assumes M1: "\<And> x y. \<lbrakk>x \<in> F; y \<in> F\<rbrakk> \<Longrightarrow> x \<otimes> y \<in> F"
+  assumes M2: "\<And> x y. \<lbrakk>x \<in> F; y \<in> F\<rbrakk> \<Longrightarrow> x \<otimes> y = y \<otimes> x"
+  assumes M3: "\<And> x y. \<lbrakk>x \<in> F; y \<in> F\<rbrakk> \<Longrightarrow> (x \<otimes> y) \<otimes> z = x \<otimes> (y \<otimes> z)"
+  assumes M4: "jedan \<in> F \<and> jedan \<noteq> nula \<and> (\<forall> x \<in> F. jedan \<otimes> x = x)"
+  assumes M5: "\<And> x. \<lbrakk>x \<in> F; x \<noteq> nula \<rbrakk> \<Longrightarrow> (inverz_puta x \<in> F \<and> x \<otimes> (inverz_puta x) = jedan)"
+
+  assumes D: "\<And> x y z. \<lbrakk>x \<in> F; y \<in> F; z \<in> F\<rbrakk> \<Longrightarrow> x \<otimes> (y \<oplus> z) = x \<otimes> y \<oplus> x \<otimes> z"
+begin
+
+text\<open>1.14 Propozicija\<close>
+proposition 
+  assumes "x \<oplus> y = x \<oplus> z"
+  shows "y = z" 
+  sorry
+proposition 
+  assumes "x \<oplus> y = x"
+  shows "y = nula" 
+  sorry
+proposition 
+  assumes "x \<oplus> y = nula"
+  shows "y = inverz_plus x" 
+  sorry
+proposition 
+  shows "inverz_plus (inverz_plus x) = x" 
+  sorry
+
+text\<open>1.15 Propozicija\<close>
+proposition 
+  assumes "x \<noteq> nula" and  "x \<otimes> y = x \<otimes> z"
+  shows "y = z"
+  sorry
+proposition 
+  assumes "x \<noteq> nula" and  "x \<otimes> y = x"
+  shows "y = jedan"
+  sorry
+proposition 
+  assumes "x \<noteq> nula" and  "x \<otimes> y = jedan"
+  shows "y = inverz_puta x"
+  sorry
+proposition 
+  assumes "x \<noteq> nula" 
+  shows "inverz_puta (inverz_puta x) = x"
+  sorry
+
+text\<open>1.16 Propozicija\<close>
+proposition "nula \<otimes> x = nula"
+  sorry
+proposition 
+  assumes "x \<noteq> nula" and "y \<noteq> nula"
+  shows "x \<otimes> y \<noteq> nula"
+  sorry
+proposition "(inverz_plus x) \<otimes> y = x \<otimes> (inverz_plus y)"
+  sorry
+proposition "(inverz_plus x) \<otimes> (inverz_plus y) = x \<otimes> y"
+  sorry
+
+end
 
 (*
 locale Uredjenje =
