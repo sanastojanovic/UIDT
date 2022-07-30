8538ded40d8cf2fad9a929ae4c5dba08ff1ddde3
Uros Ljubisavljevic <ljubisavljevicu@gmail.com>
2022-07-30 22:20:37 +0200

    proof simplified, 2 apologies left

--- uidt_2022_seminarski/mi19425_Uros_Ljubisavljevic.thy
+++ uidt_2022_seminarski/mi19425_Uros_Ljubisavljevic.thy
@@ -52,11 +52,11 @@ lemma eq1_1:
 proof-
   have "odd (4*k-3)" using assms by auto
   then have "seqX (4*k-3) = ((-1)^(((4*k-3) + 3) div 2)) * (seqX (((4*k-3) + 1) div 2))" using seqX.simps 
-    by (metis (no_types, lifting) add_self_div_2 even_iff_mod_2_eq_zero even_zero minus_one_mult_self 
+    by (metis (no_types, lifting) add_self_div_2 even_iff_mod_2_eq_zero even_zero minus_one_mult_self
         nat_1_add_1 neg_one_even_power numeral_Bit0 numeral_plus_numeral odd_even_add semiring_norm(4))
   also have "\<dots> = ((-1)^(2*k)) * (seqX ((2*k-1)))"
     by (smt (verit, del_insts) ab_semigroup_add_class.add_ac(1) add.commute add_diff_cancel_left' 
-        add_self_div_2 assms distrib_left_numeral less_eqE mult.commute mult.left_commute mult_2
+        add_self_div_2 assms distrib_left_numeral less_eqE mult.commute mult.left_commute mult_2 
         nat_1_add_1 numeral_Bit0 numeral_Bit1)
   also have "\<dots> = seqX (2*k-1)" by simp
   finally show ?thesis .
@@ -120,8 +120,10 @@ lemma S4k_eq_2Sk:
 proof-
   have "sumFst n = (\<Sum>i::nat=1..n. seqX i)" using sumFst.simps
     by (simp add: sum.atLeast_Suc_atMost sumFstS_def sumFst_eq_sumFstS)
-  also have "\<dots> = (\<Sum>i::nat=1..k. (seqX (4*i-3)) + (seqX (4*i-2)) + (seqX (4*i-1)) + (seqX (4*i)))" using assms sorry
-  also have "\<dots> = (\<Sum>i::nat=1..k. (seqX (4*i-3)) + (-seqX (4*i-3)) + (seqX (4*i-1)) + (seqX (4*i)))" using eq1_2_1 assms by simp
+  also have "\<dots> = (\<Sum>i::nat=1..k. (seqX (4*i-3)) + (seqX (4*i-2)) + (seqX (4*i-1)) + (seqX (4*i)))" 
+    using assms sorry
+  also have "\<dots> = (\<Sum>i::nat=1..k. (seqX (4*i-3)) + (-seqX (4*i-3)) + (seqX (4*i-1)) + (seqX (4*i)))" 
+    using eq1_2_1 assms by simp
   also have "\<dots> = (\<Sum>i::nat=1..k. (seqX (4*i-1)) + (seqX (4*i)))" by simp
   also have "\<dots> = (\<Sum>i::nat=1..k. (seqX (4*i)) + (seqX (4*i)))" using eq2_1 by simp
   also have "\<dots> = (\<Sum>i::nat=1..k. 2*(seqX i))" using eq2_2 eq2_3 by simp
@@ -141,14 +143,25 @@ lemma S4kp2_eq_S4k:
 proof-
   have "sumFst (n+2) = sumFst n + seqX (4*k+1) + seqX (4*k+2)" using assms by simp
   also have "\<dots> = sumFst n + seqX (4*(k+1)-3) + seqX (4*(k+1)-2)" by simp
-  also have "\<dots> = sumFst n + seqX (4*(k+1)-3) + (-seqX (4*(k+1)-3))" using eq1_2_1 le_add2 by presburger
+  also have "\<dots> = sumFst n + seqX (4*(k+1)-3) + (-seqX (4*(k+1)-3))" 
+    using eq1_2_1 le_add2 by presburger
   also have "\<dots> = sumFst n" by simp
   finally show ?thesis .
 qed
 
-\<comment>\<open> (4) \<close>
+(* helpers *)
+lemma sgnPowEven:
+  assumes "even n"
+  shows "(-1)^n = 1"
+  using assms
+proof
+  fix k :: nat
+  assume "n = 2 * k"
+  then have "(-1)^n = (-1)^(2*k)" by simp
+  also have "\<dots> = 1" using power_minus1_even[of k] sorry
+  finally show ?thesis .
+qed
 
-(* helper *)
 lemma seqXimage:
   fixes n :: nat
   assumes "n \<ge> 1"
@@ -264,6 +277,7 @@ next
   qed
 qed
 
+\<comment>\<open> (4) \<close>
 lemma Sn_eq_n_mod2:
   fixes n :: nat
   assumes "n \<ge> 1"
@@ -294,18 +308,6 @@ qed
 
 \<comment> \<open> ===== Helpers ===== \<close>
 
-lemma sgnPowEven:
-  assumes "even n"
-  shows "(-1)^n = 1"
-  using assms
-proof
-  fix k :: nat
-  assume "n = 2 * k"
-  then have "(-1)^n = (-1)^(2*k)" by simp
-  also have "\<dots> = 1" using power_minus1_even[of k] sorry
-  finally show ?thesis .
-qed
-
 lemma sumFst_four:
   assumes "i \<le> 4"
   shows "0 \<le> sumFst i"
@@ -331,58 +333,88 @@ qed
 
 lemma Si_gt_0_4k:
   fixes k :: nat
-  assumes "1 \<le> k" "i \<le> 4*k"
-  shows "0 \<le> sumFst i"
+  assumes "1 \<le> k"
+  shows "\<forall>i\<le>4*k. 0 \<le> sumFst i"
   using assms
 proof(induction k rule: nat_induct_at_least)
   case base
   then show ?case using sumFst_four by simp
 next
+  fix i :: nat
   case (Suc n)
   then show ?case
   proof-
-    have 1: "((i \<le> 4 * Suc n) \<longrightarrow> (0 \<le> sumFst i)) \<longleftrightarrow> 
-          (((i \<le> 4*n) \<longrightarrow> (0 \<le> sumFst i)) \<and> 
-          ((i = 4*n+1) \<longrightarrow> (0 \<le> sumFst i)) \<and> 
-          ((i = 4*n+2) \<longrightarrow> (0 \<le> sumFst i)) \<and> 
-          ((i = 4*n+3) \<longrightarrow> (0 \<le> sumFst i)) \<and>
-          ((i = 4*n+4) \<longrightarrow> (0 \<le> sumFst i)))"
+
+    have 1: "(\<forall>i \<le> 4 * Suc n. 0 \<le> sumFst i) \<longleftrightarrow> 
+          (\<forall>i \<le> 4*n. 0 \<le> sumFst i) \<and> 
+          (0 \<le> sumFst (4*n+1)) \<and> 
+          (0 \<le> sumFst (4*n+2)) \<and> 
+          (0 \<le> sumFst (4*n+3)) \<and>
+          (0 \<le> sumFst (4*n+4))"
     proof
-      assume "(i \<le> 4 * Suc n) \<longrightarrow> (0 \<le> sumFst i)"
-      then have "(i \<le> 4 * n + 4) \<longrightarrow> (0 \<le> sumFst i)" by simp
-      then show "((i \<le> 4*n) \<longrightarrow> (0 \<le> sumFst i)) \<and> 
-          ((i = 4*n+1) \<longrightarrow> (0 \<le> sumFst i)) \<and> 
-          ((i = 4*n+2) \<longrightarrow> (0 \<le> sumFst i)) \<and> 
-          ((i = 4*n+3) \<longrightarrow> (0 \<le> sumFst i)) \<and>
-          ((i = 4*n+4) \<longrightarrow> (0 \<le> sumFst i))" by linarith
+      assume "(\<forall>i \<le> 4 * Suc n. 0 \<le> sumFst i)"
+      show "(\<forall>i \<le> 4*n. 0 \<le> sumFst i) \<and> 
+          (0 \<le> sumFst (4*n+1)) \<and> 
+          (0 \<le> sumFst (4*n+2)) \<and> 
+          (0 \<le> sumFst (4*n+3)) \<and>
+          (0 \<le> sumFst (4*n+4))"
+      proof
+        show "\<forall>i\<le>4 * n. 0 \<le> sumFst i" using \<open>(\<forall>i \<le> 4 * Suc n. 0 \<le> sumFst i)\<close> by simp
+      next
+        show "(0 \<le> sumFst (4*n+1)) \<and> 
+          (0 \<le> sumFst (4*n+2)) \<and> 
+          (0 \<le> sumFst (4*n+3)) \<and>
+          (0 \<le> sumFst (4*n+4))" 
+          using \<open>(\<forall>i \<le> 4 * Suc n. 0 \<le> sumFst i)\<close>
+        proof-
+          from \<open>(\<forall>i \<le> 4 * Suc n. 0 \<le> sumFst i)\<close> have "(\<forall>i \<le> 4*n + 4. 0 \<le> sumFst i)" by simp
+          then show "(0 \<le> sumFst (4*n+1)) \<and> 
+          (0 \<le> sumFst (4*n+2)) \<and> 
+          (0 \<le> sumFst (4*n+3)) \<and>
+          (0 \<le> sumFst (4*n+4))" 
+            by (metis add_2_eq_Suc add_Suc_right nat_add_left_cancel_le numeral_1_eq_Suc_0 
+                numeral_2_eq_2 numeral_3_eq_3 numeral_Bit0 numerals(1) one_le_numeral)
+        qed
+      qed
     next
-      assume "(i \<le> 4 * n \<longrightarrow> 0 \<le> sumFst i) \<and>
-              (i = 4 * n + 1 \<longrightarrow> 0 \<le> sumFst i) \<and> 
-              (i = 4 * n + 2 \<longrightarrow> 0 \<le> sumFst i) \<and> 
-              (i = 4 * n + 3 \<longrightarrow> 0 \<le> sumFst i) \<and>
-              (i = 4 * n + 4 \<longrightarrow> 0 \<le> sumFst i)"
-      then have "((i \<le> 4 * n + 3) \<longrightarrow> (0 \<le> sumFst i)) \<and> (i = 4 * n + 4 \<longrightarrow> 0 \<le> sumFst i)"
-        by (metis add.right_neutral add_Suc_right le_SucE numeral_1_eq_Suc_0 numeral_2_eq_2 numeral_3_eq_3 numerals(1))
-      then show "(i \<le> 4 * Suc n) \<longrightarrow> (0 \<le> sumFst i)" by force
+      assume pp: "(\<forall>i \<le> 4*n. 0 \<le> sumFst i) \<and> 
+          (0 \<le> sumFst (4*n+1)) \<and> 
+          (0 \<le> sumFst (4*n+2)) \<and> 
+          (0 \<le> sumFst (4*n+3)) \<and>
+          (0 \<le> sumFst (4*n+4))"
+      show "(\<forall>i \<le> 4 * Suc n. 0 \<le> sumFst i)" 
+      proof-
+        have "(\<forall>i \<le> 4 * Suc n. 0 \<le> sumFst i) \<longleftrightarrow> (\<forall>i \<le> 4 * n + 4. 0 \<le> sumFst i)" by auto
+        from pp have "(\<forall>i \<le> 4 * n + 4. 0 \<le> sumFst i)"
+        proof-
+          have \<open>(0 \<le> sumFst (4*n+4))\<close> using pp by blast
+          have "(\<forall>i \<le> 4*n + 3. 0 \<le> sumFst i)" using pp
+            by (metis Suc3_eq_add_3 Suc_eq_plus1 add.commute add_2_eq_Suc' le_SucE)
+          with \<open>(0 \<le> sumFst (4*n+4))\<close> show "(\<forall>i \<le> 4 * n + 4. 0 \<le> sumFst i)"
+            by (metis add.commute add_Suc le_antisym not_less_eq_eq numeral_2_eq_2 numeral_3_eq_3
+                numeral_Bit0 numerals(1) plus_1_eq_Suc)
+        qed
+        with \<open>(\<forall>i \<le> 4 * Suc n. 0 \<le> sumFst i) \<longleftrightarrow> (\<forall>i \<le> 4 * n + 4. 0 \<le> sumFst i)\<close> 
+        show "(\<forall>i \<le> 4 * Suc n. 0 \<le> sumFst i)" by simp
+      qed   
     qed
 
-    have 10: "(i \<le> 4*n) \<longrightarrow> (0 \<le> sumFst i)" using Suc(2) by simp
+    have 10: "(\<forall>i \<le> 4*n. 0 \<le> sumFst i)" using Suc(2) by simp
 
-    have 14: "i = 4 * n + 4 \<longrightarrow> 0 \<le> sumFst i"
-    proof
-      assume "i = 4*n + 4"
-      then have "(i div 4) \<le> 4*n" using Suc by simp
-      with Suc(1) \<open>i = 4*n + 4\<close> have "i mod 4 = 0" "4 \<le> i" by auto
-      then have "sumFst i = 2*(sumFst (i div 4))" using S4k_eq_2Sk[of i]
-        by (metis Suc_eq_plus1 \<open>i = 4 * n + 4\<close> add.commute le_add2 mult.commute mult_Suc)
-      from \<open>(i div 4) \<le> 4*n\<close> have "(sumFst (i div 4)) \<ge> 0" using Suc(2) sorry
-      with \<open>sumFst i = 2*(sumFst (i div 4))\<close> show "sumFst i \<ge> 0" by simp
+    have 14: "0 \<le> sumFst (4*n+4)"
+    proof-
+      have "((4*n+4) div 4) \<le> 4*n" using Suc(1) by simp
+      then have "n+1 \<le> 4*n" by simp
+      with Suc have "0 \<le> 2*sumFst (n+1)" by (smt (z3))
+      also have "\<dots> = sumFst (4*(n+1))"
+        by (metis S4k_eq_2Sk le_add2 nonzero_mult_div_cancel_left zero_neq_numeral)
+      also have "\<dots> = sumFst (4*n+4)" by (simp add: algebra_simps)
+      finally show "0 \<le> sumFst (4*n+4)" .
     qed
-   
-    have 13: "i = 4 * n + 3 \<longrightarrow> 0 \<le> sumFst i"
-    proof
-      assume "i = 4*n + 3"
-      then have "sumFst i = sumFst (4*n + 2) + seqX (4*n + 3)" by (simp add: numeral_3_eq_3)
+
+    have 13: "0 \<le> sumFst (4*n+3)"
+    proof-
+      have "sumFst (4*n+3) = sumFst (4*n + 2) + seqX (4*n + 3)" by (simp add: numeral_3_eq_3)
       also with \<open>1 \<le> n\<close> have "\<dots> = sumFst (4*n) + seqX (4*n + 3)" using S4kp2_eq_S4k
         by simp
       also have "\<dots> = sumFst (4*n) + seqX (4*(n+1) - 1)" by (simp add: algebra_simps)
@@ -391,28 +423,25 @@ next
       also have "\<dots> = 2 * sumFst n + seqX (n + 1)" using S4k_eq_2Sk using Suc(1) by auto
       also have "\<dots> = 2 * sumFst n + sumFst (n + 1) - sumFst n" by simp
       also have "\<dots> = sumFst n + sumFst (n + 1)" by simp
-      also have "\<dots> \<ge> 0"
-      proof-
-        (* ? ? ? ? ? *)
-        have "(n+1) \<le> 4*n" using Suc(1) by simp
-        have "n \<le> 4*n" by simp
-        then have "sumFst n \<ge> 0" using Suc sorry
-        moreover with \<open>(n+1) \<le> 4*n\<close> have "sumFst (n+1) \<ge> 0" using Suc sorry
-        ultimately show ?thesis by linarith
-      qed
-      finally show ?case .
+      finally have "sumFst (4*n+3) = sumFst n + sumFst (n + 1)" .
+      
+      have "(n+1) \<le> 4*n" using Suc(1) by simp
+      have "n \<le> 4*n" by simp
+      then have "sumFst n \<ge> 0" using Suc by simp
+      moreover with \<open>(n+1) \<le> 4*n\<close> have "sumFst (n+1) \<ge> 0" using Suc by blast
+      ultimately have "sumFst n + sumFst (n + 1) \<ge> 0" by linarith
+      with \<open>sumFst (4*n+3) = sumFst n + sumFst (n + 1)\<close> show "0 \<le> sumFst (4*n+3)" by simp
     qed
 
-    have 12: "i = 4 * n + 2 \<longrightarrow> 0 \<le> sumFst i"
-    proof
-      assume "i = 4*n + 2"
-      then have "sumFst i = sumFst (4*n)" using S4kp2_eq_S4k[of i]
+    have 12: "0 \<le> sumFst (4*n + 2)"
+    proof-
+      have "sumFst (4*n + 2) = sumFst (4*n)"
         using S4kp2_eq_S4k Suc(1) by simp
-      also have "\<dots> \<ge> 0" using Suc sorry
-      finally show ?case .
+      also have "\<dots> \<ge> 0" using Suc by simp
+      finally show "0 \<le> sumFst (4*n + 2)" by simp
     qed
 
-    have 11: "i = 4 * n + 1 \<longrightarrow> 0 \<le> sumFst i"   
+    have 11: "0 \<le> sumFst (4*n+1)"
     proof(cases "n mod 2 = 0")
       case True
       then show ?thesis
@@ -432,7 +461,9 @@ next
           have "seqX (4*n + 1) = seqX (4*(n+1) - 3)" by (simp add: algebra_simps)
           also have "\<dots> = seqX (2*(n+1) - 1)" using eq1_1[where k = "n+1"] by simp
           also with \<open>(2*(n+1) -1) mod 2 = 1\<close> have "\<dots> = ((-1)^((2*(n+1) - 1 + 3) div 2)) * (seqX ((2*(n+1) - 1 + 1) div 2))" 
-            by (metis Nat.le_imp_diff_is_add Suc.hyps bits_mod_0 calculation diff_add_inverse2 diff_le_self diff_self_eq_0 mult_eq_self_implies_10 not_numeral_le_zero numeral_Bit0 numeral_One seqX.elims zero_neq_numeral)
+            by (metis Nat.le_imp_diff_is_add Suc.hyps bits_mod_0 calculation diff_add_inverse2 
+                diff_le_self diff_self_eq_0 mult_eq_self_implies_10 not_numeral_le_zero numeral_Bit0
+                numeral_One seqX.elims zero_neq_numeral)
           also have "\<dots> = ((-1)^((2*(n + 1) + 2) div 2)) * (seqX ((2*n+2) div 2))" by (simp add: algebra_simps)
           also with \<open>((-1)^((2*(n + 1) + 2) div 2)) = 1\<close> have "\<dots> = (seqX ((2*n+2) div 2))"
             by (smt (verit) left_minus_one_mult_self power_add_numeral2 power_one)
@@ -440,16 +471,18 @@ next
           finally show ?thesis .
         qed        
 
-        show "i = 4 * n + 1 \<longrightarrow> 0 \<le> sumFst i"
-        proof
-          assume "i = 4*n + 1"
-          then have "sumFst i = sumFst (4*n) + seqX (4*n + 1)" by simp
+        show "0 \<le> sumFst (4 * n + 1)"
+        proof-
+          have "sumFst (4*n + 1) = sumFst (4*n) + seqX (4*n + 1)" by simp
           also with \<open>seqX (4 * n + 1) = seqX (n + 1)\<close> have "\<dots> = sumFst (4*n) + seqX (n + 1)" by simp
           also have "\<dots> = 2*sumFst n + seqX (n + 1)" using S4k_eq_2Sk Suc(1) by simp
           also have "\<dots> = 2*sumFst n + sumFst (n + 1) - sumFst n" by simp
           also have "\<dots> = sumFst n + sumFst (n + 1)" by simp
-          also have "\<dots> \<ge> 0" using Suc sorry
-          finally show "sumFst i \<ge> 0" .
+          also have "\<dots> \<ge> 0" using Suc
+            by (smt (verit) add_increasing2 add_mono_thms_linordered_semiring(2) distrib_left 
+                distrib_right le_add_same_cancel2 le_iff_add mult.comm_neutral mult.commute num_double 
+                numeral_times_numeral one_add_one)
+          finally show "sumFst (4*n + 1) \<ge> 0" .
         qed
       qed
     next
@@ -457,27 +490,28 @@ next
       then show ?thesis
       proof-
         assume "n mod 2 \<noteq> 0"
-        show "i = 4 * n + 1 \<longrightarrow> 0 \<le> sumFst i"
-        proof
+        show "0 \<le> sumFst (4*n + 1)"
+        proof-
           have "(sumFst n) mod 2 = 1" using Sn_eq_n_mod2
             using False by auto
-          then have "sumFst n \<ge> 1" sorry
+          then have "sumFst n \<ge> 1" using Suc
+            by (metis mult_le_mono1 nat_mult_1 one_le_numeral 
+                unique_euclidean_semiring_numeral_class.mod_less_eq_dividend)
 
           have "sumFst(4*n) = 2*sumFst(n)" using S4k_eq_2Sk Suc(1) by simp
           also with \<open>sumFst n \<ge> 1\<close> have "\<dots> \<ge> 2" by simp 
           finally have "sumFst (4*n) \<ge> 2" .
 
-          assume "i = 4*n +1"
-          then have "sumFst i = sumFst (4*n) + seqX (4*n + 1)" by simp
+          have "sumFst (4*n + 1) = sumFst (4*n) + seqX (4*n + 1)" by simp
           also with \<open>sumFst (4*n) \<ge> 2\<close> have "\<dots> \<ge> 0" using seqXimage
             by (smt (verit, del_insts) le_add_same_cancel2 zero_le)
-          finally show "sumFst i \<ge> 0" .
+          finally show "sumFst (4*n + 1) \<ge> 0" .
         qed
       qed
     qed
+  
+    show ?case using 1 10 11 12 13 14 by blast
 
-    show ?case using 1 10 11 12 13 14
-      using Suc.prems by blast
   qed
 qed
 
