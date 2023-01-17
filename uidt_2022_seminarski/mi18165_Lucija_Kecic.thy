3f517a3e0bf68e7ac02725dc12afe1ca0845199f
floxinoxinihiliplification <57249515+floxinoxinihiliplification@users.noreply.github.com>
2023-01-17 20:25:28 +0100

    Ispravljena funkcija min

--- uidt_2022_seminarski/mi18165_Lucija_Kecic.thy
+++ uidt_2022_seminarski/mi18165_Lucija_Kecic.thy
@@ -4,16 +4,16 @@ theory mi18165_Lucija_Kecic
 begin
 
 (*Let a, b and c be positive real numbers
-satisfying min(a+b, b+c, c+a) > \<surd>2 and
+satisfying min(a+b, b+c, c+a) > √2 and
 a^2+b^2+c^2=3
 Prove that
-a/(b+c-a)^2 + b/(c+a-b)^2+c/(a+b-c)^2\<ge>3/(a*b*c)^2
+a/(b+c-a)^2 + b/(c+a-b)^2+c/(a+b-c)^2≥3/(a*b*c)^2
 *)
 
-fun min :: "real \<Rightarrow> real \<Rightarrow> real \<Rightarrow> real" where
+fun min :: "real ⇒ real ⇒ real ⇒ real" where
   "min a b c =
-	(if a>b | c>b then b
-	else if a>c | b>c then c
+	(if a≥b | c≥b then b
+	else if a≥c | b≥c then c
 	else a)"
 
 lemma problem:
@@ -22,25 +22,25 @@ lemma problem:
   assumes "min(a+b)(b+c)(c+a) > sqrt(2)"
 (*ovaj uslov znaci da je svaki zbir > 1/2*)
   assumes "a^2 + b^2 + c^2 = 3"
-  shows "a/(b+c-a)^2 + b/(c+a-b)^2 + c/(a+b-c)^2 \<ge> 3/(a*b*c)^2"
+  shows "a/(b+c-a)^2 + b/(c+a-b)^2 + c/(a+b-c)^2 ≥ 3/(a*b*c)^2"
   using assms
   oops
 
 lemma am_qm_inequality:
-  shows "sqrt((a^2 + b^2)/2)\<ge>(a+b)/2"
+  shows "sqrt((a^2 + b^2)/2)≥(a+b)/2"
 proof -
   have "(a+b)^2 = a^2 + 2*a*b + b^2"
    (* by sledgehammer*)
 	by (simp add: power2_sum)
-  then have "(a+b)^2 \<le> 2*(a^2+b^2)"
+  then have "(a+b)^2 ≤ 2*(a^2+b^2)"
    (* by sledgehammer*)
 	by (smt (verit) sum_squares_bound)
-  then have "a+b \<le> sqrt(2*(a^2+b^2))"
+  then have "a+b ≤ sqrt(2*(a^2+b^2))"
    (* by sledgehammer*)
 	using real_le_rsqrt by presburger
-  then have "a+b \<le> sqrt(2)*sqrt(a^2+b^2)"
+  then have "a+b ≤ sqrt(2)*sqrt(a^2+b^2)"
 	using real_sqrt_mult by metis
-  then show "(a+b)/2 \<le> sqrt((a^2+b^2)/2)"
+  then show "(a+b)/2 ≤ sqrt((a^2+b^2)/2)"
 	(*by sledgehammer*)
 	by (smt (z3) field_sum_of_halves power2_eq_square real_divide_square_eq real_sqrt_divide real_sqrt_pow2)
 qed
@@ -53,7 +53,7 @@ lemma sq_gt_one:
   assumes "b+c > sqrt(2)"
   shows "b^2 + c^2 > 1"
 proof -
-  have "sqrt((b^2+c^2)/2) \<ge> (b+c)/2"
+  have "sqrt((b^2+c^2)/2) ≥ (b+c)/2"
 	using am_qm_inequality by auto
   then have "sqrt((b^2+c^2)/2) > sqrt(2)/2"
 	using assms by simp (*sledgehammer*)
@@ -88,14 +88,14 @@ lemma equation_gt_zero:
   using assms by auto
 
 (* HOLDER:
-x1^(p+1)/y1^p + ... + xn^(p+1)/yn^p \<ge> (x1+...+xn)^(p+1)/(y1+...+yn)^p
+x1^(p+1)/y1^p + ... + xn^(p+1)/yn^p ≥ (x1+...+xn)^(p+1)/(y1+...+yn)^p
 u nasem slucaju p=2, n=3
 *)
 
 lemma holders_inequality:
   fixes x1 x2 x3 y1 y2 y3 :: "real"
   assumes "x1>0" "x2>0" "x3>0" "y1>0" "y2>0" "y3>0"
-  shows "x1^3/y1^2 + x2^3/y2^2 + x3^3/y3^2 \<ge> (x1+x2+x3)^3/(y1+y2+y3)^2"
+  shows "x1^3/y1^2 + x2^3/y2^2 + x3^3/y3^2 ≥ (x1+x2+x3)^3/(y1+y2+y3)^2"
   using assms
   sorry
 
@@ -103,7 +103,7 @@ lemma apply_holder_left_mid:
   fixes a b c :: "real"
   assumes "a > 0" "b > 0" "c > 0"
   assumes "a^2 + b^2 + c^2 = 3"
-  shows  "((a^2)^3) / (a^5 * (b + c - a)^2) + ((b^2)^3) / (b^5 * (a + c - b)^2)  + ((c^2)^3) / (c^5 * (a + b - c)^2) \<ge> (a^2 + b^2 + c^2)^3 / (sqrt(a^5) * (b + c - a) + sqrt(b^5) * (a + c - b) + sqrt(c^5) * (a + b - c))^2"
+  shows  "((a^2)^3) / (a^5 * (b + c - a)^2) + ((b^2)^3) / (b^5 * (a + c - b)^2)  + ((c^2)^3) / (c^5 * (a + b - c)^2) ≥ (a^2 + b^2 + c^2)^3 / (sqrt(a^5) * (b + c - a) + sqrt(b^5) * (a + c - b) + sqrt(c^5) * (a + b - c))^2"
   using assms
   sorry
 
@@ -116,7 +116,7 @@ lemma apply_holder_left:
   fixes a b c :: "real"
   assumes "a > 0" "b > 0" "c > 0"
   assumes "a^2 + b^2 + c^2 = 3"
-  shows "a/(b+c-a)^2+b/(a+c-b)^2+c/(a+b-c)^2 \<ge> 27/(sqrt(a^5)*(b+c-a)+sqrt(b^5)*(a+c-b)+sqrt(c^5)*(a+b-c))^2"
+  shows "a/(b+c-a)^2+b/(a+c-b)^2+c/(a+b-c)^2 ≥ 27/(sqrt(a^5)*(b+c-a)+sqrt(b^5)*(a+c-b)+sqrt(c^5)*(a+b-c))^2"
 proof -
   have fraca: "a/(b+c-a)^2 = ((a^2)^3)/(a^5*(b+c-a)^2)"
 	using fraction_multiplication by auto
@@ -126,11 +126,11 @@ proof -
 	using fraction_multiplication by auto
   from fraca fracb fracc have eq1: "a/(b+c-a)^2+b/(a+c-b)^2+c/(a+b-c)^2 = ((a^2)^3)/(a^5*(b+c-a)^2)+((b^2)^3)/(b^5*(a+c-b)^2)+((c^2)^3)/(c^5*(a+b-c)^2)"
 	by simp
-  also have eq2: "((a^2)^3)/(a^5*(b+c-a)^2)+(b^2)^3/(b^5*(a+c-b)^2)+(c^2)^3/(c^5*(a+b-c)^2) \<ge> (a^2+b^2+c^2)^3/(sqrt(a^5)*(b+c-a)+sqrt(b^5)*(a+c-b)+sqrt(c^5)*(a+b-c))^2"
+  also have eq2: "((a^2)^3)/(a^5*(b+c-a)^2)+(b^2)^3/(b^5*(a+c-b)^2)+(c^2)^3/(c^5*(a+b-c)^2) ≥ (a^2+b^2+c^2)^3/(sqrt(a^5)*(b+c-a)+sqrt(b^5)*(a+c-b)+sqrt(c^5)*(a+b-c))^2"
 	using assms apply_holder_left_mid by metis
   have final: "(a^2+b^2+c^2)^3/(sqrt(a^5)*(b+c-a)+sqrt(b^5)*(c+a-b)+sqrt(c^5)*(a+b-c))^2 = 27/(sqrt(a^5)*(b+c-a)+sqrt(b^5)*(c+a-b)+sqrt(c^5)*(a+b-c))^2"
 	using assms by auto
-  from eq1 eq2 final show "a/(b+c-a)^2+b/(a+c-b)^2+c/(a+b-c)^2 \<ge> 27/(sqrt(a^5)*(b+c-a)+sqrt(b^5)*(a+c-b)+sqrt(c^5)*(a+b-c))^2"
+  from eq1 eq2 final show "a/(b+c-a)^2+b/(a+c-b)^2+c/(a+b-c)^2 ≥ 27/(sqrt(a^5)*(b+c-a)+sqrt(b^5)*(a+c-b)+sqrt(c^5)*(a+b-c))^2"
 	by (smt (verit, del_insts))
 qed
 
@@ -138,28 +138,28 @@ lemma schur:
   fixes a b c :: "real"
   assumes "a>0" "b>0" "c>0"
   assumes "a^2+b^2+c^2 = 3"
-  assumes "a \<ge> b" "b \<ge> c"
-  shows "sqrt(a^3)*(a-b)*(a-c)+sqrt(b^3)*(b-a)*(b-c)+sqrt(c^3)*(c-a)*(c-b) \<ge> 0"
+  assumes "a ≥ b" "b ≥ c"
+  shows "sqrt(a^3)*(a-b)*(a-c)+sqrt(b^3)*(b-a)*(b-c)+sqrt(c^3)*(c-a)*(c-b) ≥ 0"
 proof -
-  have a1: "a-b \<ge> 0"
+  have a1: "a-b ≥ 0"
 	using assms by auto
-  have a2: "a-c \<ge> 0"
+  have a2: "a-c ≥ 0"
 	using assms by auto
-  have a3: "(a-b)*(a-c) \<ge> 0"
+  have a3: "(a-b)*(a-c) ≥ 0"
 	using a1 a2 mult_nonneg_nonneg by blast
-  then have "sqrt(a^3)*(a-b)*(a-c) \<ge> 0"
+  then have "sqrt(a^3)*(a-b)*(a-c) ≥ 0"
 	using a1 a2 assms(1) by auto
-  have b1: "b-c \<ge> 0"
+  have b1: "b-c ≥ 0"
 	using assms by auto
-  have b2:  "b-a \<le> 0"
+  have b2:  "b-a ≤ 0"
 	using assms by auto
-  have b3: "(b-a)*(b-c) \<le> 0"
+  have b3: "(b-a)*(b-c) ≤ 0"
 	using b1 b2 by (simp add: mult_nonneg_nonpos2)
-  then have "sqrt(b^3)*(b-a)*(b-c) \<le> 0"
+  then have "sqrt(b^3)*(b-a)*(b-c) ≤ 0"
 	using b1 b2 b3 assms(1) by (smt (verit, del_insts) assms(2) mult_nonneg_nonpos2 real_sqrt_le_0_iff zero_less_mult_pos zero_less_power)
-  then have "sqrt(b^3)*(-(a-b))*(b-c) \<le> 0"
+  then have "sqrt(b^3)*(-(a-b))*(b-c) ≤ 0"
 	by simp
-  then have "-sqrt(b^3)*(a-b)*(b-c) \<le> 0"
+  then have "-sqrt(b^3)*(a-b)*(b-c) ≤ 0"
 	by linarith
   have eq1: "sqrt(a^3)*(a-b)*(a-c)+sqrt(b^3)*(b-a)*(b-c) = sqrt(a^3)*(a-b)*(a-c)-sqrt(b^3)*(a-b)*(b-c)"
 	by (smt (verit, ccfv_SIG) mult_minus_left mult_minus_right)
@@ -167,21 +167,21 @@ proof -
 	by (simp add: mult.commute right_diff_distrib')
   then have eq3: "sqrt(a^3)*(a-b)*(a-c)+sqrt(b^3)*(b-a)*(b-c) = (a-b)*(sqrt(a^3)*(a-c)-sqrt(b^3)*(b-c))"
 	using eq1 by presburger
-  have "a-c \<ge> b-c"
+  have "a-c ≥ b-c"
 	using assms a2 b2 by auto
-  then have "sqrt(a^3)*(a-c) \<ge> sqrt(b^3)*(b-c)"
+  then have "sqrt(a^3)*(a-c) ≥ sqrt(b^3)*(b-c)"
 	by (smt (verit, ccfv_SIG) assms(2) b1 mult_strict_mono not_gr_zero power_strict_mono real_sqrt_le_0_iff real_sqrt_less_iff zero_less_power zero_neq_numeral)
-  then have ab: "(a-b)*(sqrt(a^3)*(a-c)-sqrt(b^3)*(b-c)) \<ge> 0"
+  then have ab: "(a-b)*(sqrt(a^3)*(a-c)-sqrt(b^3)*(b-c)) ≥ 0"
 	using a1 by auto
-  have c1: "c-a \<le> 0"
+  have c1: "c-a ≤ 0"
 	using assms by auto
-  have c2: "c-b \<le> 0"
+  have c2: "c-b ≤ 0"
 	using assms by auto
-  have c3: "(c-a)*(c-b) \<ge> 0"
+  have c3: "(c-a)*(c-b) ≥ 0"
 	using c1 c2 mult_nonpos_nonpos by blast
-  then have c4: "sqrt(c^3)*(c-a)*(c-b) \<ge> 0"
+  then have c4: "sqrt(c^3)*(c-a)*(c-b) ≥ 0"
 	using c1 c2 by (smt (verit, del_insts) assms(3) mult_nonpos_nonpos real_sqrt_le_0_iff zero_less_mult_pos zero_less_power)
-  show "sqrt(a^3)*(a-b)*(a-c)+sqrt(b^3)*(b-a)*(b-c)+sqrt(c^3)*(c-a)*(c-b) \<ge> 0"
+  show "sqrt(a^3)*(a-b)*(a-c)+sqrt(b^3)*(b-a)*(b-c)+sqrt(c^3)*(c-a)*(c-b) ≥ 0"
 	using assms ab c4 eq3 by auto
 qed
 
@@ -234,10 +234,10 @@ lemma rewritten_schur:
   fixes a b c :: "real"
   assumes "a>0" "b>0" "c>0"
   assumes "a^2+b^2+c^2 = 3"
-  assumes "a \<ge> b" "b \<ge> c"
-  shows "sqrt(a^5)*(b+c-a)+sqrt(b^5)*(c+a-b)+sqrt(c^5)*(a+b-c) \<le> a*b*c*(sqrt(a)+sqrt(b)+sqrt(c))"
+  assumes "a ≥ b" "b ≥ c"
+  shows "sqrt(a^5)*(b+c-a)+sqrt(b^5)*(c+a-b)+sqrt(c^5)*(a+b-c) ≤ a*b*c*(sqrt(a)+sqrt(b)+sqrt(c))"
 proof -
-  have schur1: "sqrt(a^3)*(a-b)*(a-c)+sqrt(b^3)*(b-a)*(b-c)+sqrt(c^3)*(c-a)*(c-b) \<ge> 0"
+  have schur1: "sqrt(a^3)*(a-b)*(a-c)+sqrt(b^3)*(b-a)*(b-c)+sqrt(c^3)*(c-a)*(c-b) ≥ 0"
 	using assms schur by auto
   have a1: "sqrt(a^3)*(a-b)*(a-c) = sqrt(a^3)*(a^2-a*b-a*c+b*c)"
 	using assms h1 by auto
@@ -253,7 +253,7 @@ proof -
 	using assms h2 by auto
   then have abc1: "sqrt(a^3)*(a-b)*(a-c)+sqrt(b^3)*(b-a)*(b-c)+sqrt(c^3)*(c-a)*(c-b) = a*sqrt(a^5)-b*sqrt(a^5)-c*sqrt(a^5)+a*b*c*sqrt(a)+b*sqrt(b^5)-a*sqrt(b^5)-c*sqrt(b^5)+a*b*c*sqrt(b)+c*sqrt(c^5)-a*sqrt(c^5)-b*sqrt(c^5)+a*b*c*sqrt(c)"
 	using assms a2 b2 c2 by (smt (verit) a1 b1 c1 mult.commute)
-  then have "a*sqrt(a^5)-b*sqrt(a^5)-c*sqrt(a^5)+a*b*c*sqrt(a)+b*sqrt(b^5)-a*sqrt(b^5)-c*sqrt(b^5)+a*b*c*sqrt(b)+c*sqrt(c^5)-a*sqrt(c^5)-b*sqrt(c^5)+a*b*c*sqrt(c) \<ge> 0"
+  then have "a*sqrt(a^5)-b*sqrt(a^5)-c*sqrt(a^5)+a*b*c*sqrt(a)+b*sqrt(b^5)-a*sqrt(b^5)-c*sqrt(b^5)+a*b*c*sqrt(b)+c*sqrt(c^5)-a*sqrt(c^5)-b*sqrt(c^5)+a*b*c*sqrt(c) ≥ 0"
 	using schur1 by presburger
   have abc: "a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)) = a*b*c*sqrt(a)+a*b*c*sqrt(b)+a*b*c*sqrt(c)"
 	using assms by (smt (verit, best) left_diff_distrib mult.commute)
@@ -267,11 +267,11 @@ proof -
 	using a3 abc b3 by fastforce
   then have "a*b*c*(sqrt(a)+sqrt(b)+sqrt(c))+sqrt(a^5)*(a-b-c)+sqrt(b^5)*(b-a-c)+sqrt(c^5)*(c-a-b) = a*b*c*(sqrt(a)+sqrt(b)+sqrt(c))-sqrt(a^5)*(b+c-a)-sqrt(b^5)*(a+c-b)-sqrt(c^5)*(a+b-c)"
 	by (smt (verit) mult_minus_right)
-  then have "a*b*c*(sqrt(a)+sqrt(b)+sqrt(c))-sqrt(a^5)*(b+c-a)-sqrt(b^5)*(a+c-b)-sqrt(c^5)*(a+b-c) \<ge> 0"
+  then have "a*b*c*(sqrt(a)+sqrt(b)+sqrt(c))-sqrt(a^5)*(b+c-a)-sqrt(b^5)*(a+c-b)-sqrt(c^5)*(a+b-c) ≥ 0"
 	using abc1 abc2 schur1 by presburger
-  then have "a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)) \<ge> sqrt(a^5)*(b+c-a)+sqrt(b^5)*(a+c-b)+sqrt(c^5)*(a+b-c)"
+  then have "a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)) ≥ sqrt(a^5)*(b+c-a)+sqrt(b^5)*(a+c-b)+sqrt(c^5)*(a+b-c)"
 	by linarith
-  then show "sqrt(a^5)*(b+c-a)+sqrt(b^5)*(c+a-b)+sqrt(c^5)*(a+b-c) \<le> a*b*c*(sqrt(a)+sqrt(b)+sqrt (c))"
+  then show "sqrt(a^5)*(b+c-a)+sqrt(b^5)*(c+a-b)+sqrt(c^5)*(a+b-c) ≤ a*b*c*(sqrt(a)+sqrt(b)+sqrt (c))"
 	by (smt (verit))
 qed
 
@@ -279,22 +279,22 @@ lemma am_fpm:
   fixes a b c :: "real"
   assumes "a>0" "b>0" "c>0"
   assumes "a^2+b^2+c^2 = 3"
-  shows "((sqrt(a)+sqrt(b)+sqrt(c))/3)^4 \<le> (a^2+b^2+c^2)/3"
+  shows "((sqrt(a)+sqrt(b)+sqrt(c))/3)^4 ≤ (a^2+b^2+c^2)/3"
   sorry
 
 lemma sq_sum_lt_three:
   fixes a b c :: "real"
   assumes "a>0" "b>0" "c>0"
   assumes "a^2+b^2+c^2 = 3"
-  shows "sqrt(a)+sqrt(b)+sqrt(c) \<le> 3"
+  shows "sqrt(a)+sqrt(b)+sqrt(c) ≤ 3"
 proof -
-  have "((sqrt(a)+sqrt(b)+sqrt(c))/3)^4 \<le> (a^2+b^2+c^2)/3"
+  have "((sqrt(a)+sqrt(b)+sqrt(c))/3)^4 ≤ (a^2+b^2+c^2)/3"
 	using assms am_fpm by auto
-  then have "((sqrt(a)+sqrt(b)+sqrt(c))/3)^4 \<le> 1"
+  then have "((sqrt(a)+sqrt(b)+sqrt(c))/3)^4 ≤ 1"
 	using assms by auto
-  then have "(sqrt(a)+sqrt(b)+sqrt(c))/3 \<le> 1"
+  then have "(sqrt(a)+sqrt(b)+sqrt(c))/3 ≤ 1"
 	by (smt (verit, ccfv_threshold) mult.right_neutral nat_1_add_1 numeral_Bit0 one_le_power power2_eq_square power2_sum power_mult real_sqrt_abs real_sqrt_one)
-  then show "sqrt(a)+sqrt(b)+sqrt(c) \<le> 3"
+  then show "sqrt(a)+sqrt(b)+sqrt(c) ≤ 3"
 	by simp
 qed
 
@@ -302,23 +302,23 @@ lemma left:
   fixes a b c :: "real"
   assumes "a>0" "b>0" "c>0"
   assumes "a^2+b^2+c^2 = 3"
-  assumes "a \<ge> b" "b \<ge> c"
+  assumes "a ≥ b" "b ≥ c"
   assumes "sqrt(a^5)*(b+c-a)+sqrt(b^5)*(c+a-b)+sqrt(c^5)*(a+b-c) > 0"
   assumes "a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)) > 0"
   assumes "a+b-c > 0" "b+c-a > 0" "c+a-b > 0"
-  shows "a/(b+c-a)^2+b/(a+c-b)^2+c/(a+b-c)^2 \<ge> 27/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2"
+  shows "a/(b+c-a)^2+b/(a+c-b)^2+c/(a+b-c)^2 ≥ 27/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2"
 proof -
-  have l1: "a/(b+c-a)^2+b/(a+c-b)^2+c/(a+b-c)^2 \<ge> 27/(sqrt(a^5)*(b+c-a)+sqrt(b^5)*(a+c-b)+sqrt(c^5)*(a+b-c))^2"
+  have l1: "a/(b+c-a)^2+b/(a+c-b)^2+c/(a+b-c)^2 ≥ 27/(sqrt(a^5)*(b+c-a)+sqrt(b^5)*(a+c-b)+sqrt(c^5)*(a+b-c))^2"
 	using assms apply_holder_left by auto
-  have "sqrt(a^5)*(b+c-a)+sqrt(b^5)*(c+a-b)+sqrt(c^5)*(a+b-c) \<le> a*b*c*(sqrt(a)+sqrt(b)+sqrt(c))"
+  have "sqrt(a^5)*(b+c-a)+sqrt(b^5)*(c+a-b)+sqrt(c^5)*(a+b-c) ≤ a*b*c*(sqrt(a)+sqrt(b)+sqrt(c))"
 	using assms rewritten_schur by auto
-  then have "(sqrt(a^5)*(b+c-a)+sqrt(b^5)*(c+a-b)+sqrt(c^5)*(a+b-c))^2 \<le> (a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2"
+  then have "(sqrt(a^5)*(b+c-a)+sqrt(b^5)*(c+a-b)+sqrt(c^5)*(a+b-c))^2 ≤ (a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2"
 	using assms by auto
-  then have "1/(sqrt(a^5)*(b+c-a)+sqrt(b^5)*(c+a-b)+sqrt(c^5)*(a+b-c))^2 \<ge> 1/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2"
+  then have "1/(sqrt(a^5)*(b+c-a)+sqrt(b^5)*(c+a-b)+sqrt(c^5)*(a+b-c))^2 ≥ 1/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2"
 	using assms rewritten_schur by (smt (verit) frac_le zero_less_power)
-  then have l2:  "27/(sqrt(a^5)*(b+c-a)+sqrt(b^5)*(c+a-b)+sqrt(c^5)*(a+b-c))^2 \<ge> 27/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2"
+  then have l2:  "27/(sqrt(a^5)*(b+c-a)+sqrt(b^5)*(c+a-b)+sqrt(c^5)*(a+b-c))^2 ≥ 27/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2"
 	by auto
-  from l1 l2 show "a/(b+c-a)^2+b/(a+c-b)^2+c/(a+b-c)^2 \<ge> 27/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2"
+  from l1 l2 show "a/(b+c-a)^2+b/(a+c-b)^2+c/(a+b-c)^2 ≥ 27/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2"
 	using assms by (smt (verit))
 qed
 
@@ -326,26 +326,26 @@ lemma right:
   fixes a b c :: "real"
   assumes "a>0" "b>0" "c>0"
   assumes "a^2+b^2+c^2 = 3"
-  assumes "a \<ge> b" "b \<ge> c"
+  assumes "a ≥ b" "b ≥ c"
   assumes "sqrt(a) > 0" "sqrt(b) > 0" "sqrt(c) > 0"
   assumes "sqrt(a)+sqrt(b)+sqrt(c) > 0"
   assumes "a*b*c > 0"
   assumes "a+b-c > 0" "b+c-a > 0" "c+a-b > 0"
-  shows "27/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2 \<ge> 3/(a*b*c)^2"
+  shows "27/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2 ≥ 3/(a*b*c)^2"
 proof -
-  have "sqrt(a)+sqrt(b)+sqrt(c) \<le> 3"
+  have "sqrt(a)+sqrt(b)+sqrt(c) ≤ 3"
 	using assms sq_sum_lt_three by auto
-  then have "a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)) \<le> 3*a*b*c"
+  then have "a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)) ≤ 3*a*b*c"
 	using assms by simp
-  then have "(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2 \<le> (3*a*b*c)^2"
+  then have "(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2 ≤ (3*a*b*c)^2"
 	using assms by auto
-  then have "(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2 \<le> 9*(a*b*c)^2"
+  then have "(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2 ≤ 9*(a*b*c)^2"
 	using assms by (simp add: power2_eq_square)
-  then have "1/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2 \<ge> 1/(9*(a*b*c)^2)"
+  then have "1/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2 ≥ 1/(9*(a*b*c)^2)"
 	using assms by (simp add: frac_le)
-  then have "27/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2 \<ge> 27/(9*(a*b*c)^2)"
+  then have "27/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2 ≥ 27/(9*(a*b*c)^2)"
 	by auto
-  then show "27/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2 \<ge> 3/(a*b*c)^2"
+  then show "27/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2 ≥ 3/(a*b*c)^2"
 	by auto
 qed
 
@@ -353,19 +353,19 @@ lemma solution:
   fixes a b c :: "real"
   assumes "a>0" "b>0" "c>0"
   assumes "a^2+b^2+c^2 = 3"
-  assumes "a \<ge> b" "b \<ge> c"
+  assumes "a ≥ b" "b ≥ c"
   assumes "sqrt(a) > 0" "sqrt(b) > 0" "sqrt(c) > 0"
   assumes "sqrt(a)+sqrt(b)+sqrt(c) > 0"
   assumes "a*b*c > 0"
   assumes "a+b-c > 0" "b+c-a > 0" "c+a-b > 0"
   assumes "sqrt(a^5)*(b+c-a)+sqrt(b^5)*(c+a-b)+sqrt(c^5)*(a+b-c) > 0"
-  shows "a/(b+c-a)^2 + b/(c+a-b)^2 + c/(a+b-c)^2 \<ge> 3/(a*b*c)^2"
+  shows "a/(b+c-a)^2 + b/(c+a-b)^2 + c/(a+b-c)^2 ≥ 3/(a*b*c)^2"
 proof -
-  have l: "a/(b+c-a)^2+b/(a+c-b)^2+c/(a+b-c)^2 \<ge> 27/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2"
+  have l: "a/(b+c-a)^2+b/(a+c-b)^2+c/(a+b-c)^2 ≥ 27/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2"
 	using assms left by auto
-  have r: "27/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2 \<ge> 3/(a*b*c)^2"
+  have r: "27/(a*b*c*(sqrt(a)+sqrt(b)+sqrt(c)))^2 ≥ 3/(a*b*c)^2"
 	using assms right by auto
-  from l r show "a/(b+c-a)^2 + b/(c+a-b)^2 + c/(a+b-c)^2 \<ge> 3/(a*b*c)^2"
+  from l r show "a/(b+c-a)^2 + b/(c+a-b)^2 + c/(a+b-c)^2 ≥ 3/(a*b*c)^2"
 	using assms by (smt (verit))
 qed
 
